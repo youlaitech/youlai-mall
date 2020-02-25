43ffadf6586dbf7de617585efaef4207ff6d924c
fly <1490493387@qq.com>
2020-02-25 16:21:12 +0800

    docker集成maven插件

--- fly-service/fly-system/src/main/java/com/fly/system/config/DruidConfig.java
+++ fly-service/fly-system/src/main/java/com/fly/system/config/DruidConfig.java
@@ -1,76 +1,32 @@
 package com.fly.system.config;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import com.fly.common.datasource.DynamicDataSource;
-import com.fly.common.enums.DataSourceType;
-import com.netflix.discovery.converters.Auto;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.type.JdbcType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class DruidConfig {
 
-    @Autowired
-    private PaginationInterceptor paginationInterceptor;
-
-    @Bean(name = "mysql")
-    @ConfigurationProperties(prefix = "spring.datasource.druid.mysql")
-    public DataSource mysqlDataSource(){
-        return  DruidDataSourceBuilder.create().build();
-    }
-
-    @Bean(name = "oracle")
-    @ConfigurationProperties(prefix = "spring.datasource.druid.oracle")
-    public DataSource oracleDataSource(){
-        return  DruidDataSourceBuilder.create().build();
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean("master")
+    public DataSource masterDataSource() {
+        return new DruidDataSource();
     }
 
 
-    @Bean
-    @Primary
-    public DataSource dynamicDataSource(
-            @Qualifier("mysql") DataSource mysqlDataSource,
-            @Qualifier("oracle") DataSource oracleDataSource
-    ){
 
-        Map<Object,Object> targetDataSources=new HashMap<>();
-        targetDataSources.put(DataSourceType.mysql.getValue(),mysqlDataSource);
-        targetDataSources.put(DataSourceType.oracle.getValue(),oracleDataSource);
+    @Value("{}")
 
-        DynamicDataSource dynamicDataSource=new DynamicDataSource();
-        dynamicDataSource.setTargetDataSources(targetDataSources);
-        dynamicDataSource.setDefaultTargetDataSource(mysqlDataSource);
-        return dynamicDataSource;
+    public DataSource slaveDataSource(){
+        DruidDataSource slaveDataSource=new DruidDataSource();
+        slaveDataSource.setUrl();
+
+        return slaveDataSource;
     }
 
 
-    @Bean("sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        MybatisSqlSessionFactoryBean sqlSessionFactory=new MybatisSqlSessionFactoryBean();
-        sqlSessionFactory.setDataSource(dynamicDataSource(mysqlDataSource(),oracleDataSource()));
-
-        MybatisConfiguration configuration=new MybatisConfiguration();
-        configuration.setJdbcTypeForNull(JdbcType.NULL);
-        configuration.setMapUnderscoreToCamelCase(true);
-        configuration.setCacheEnabled(false);
-        sqlSessionFactory.setConfiguration(configuration);
-        sqlSessionFactory.setPlugins(new Interceptor[]{
-                paginationInterceptor
-        });
-        return sqlSessionFactory.getObject();
-    }
 }
