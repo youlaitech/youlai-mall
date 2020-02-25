4762714eb82a266d3c27193e16d0a9770bbb4257
fly <1490493387@qq.com>
2020-02-25 16:21:12 +0800

    docker集成maven插件

--- fly-service/fly-system/src/main/java/com/fly/system/service/ITUserService.java
+++
@@ -1,7 +0,0 @@
-package com.fly.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.fly.system.pojo.entity.TUser;
-
-public interface ITUserService  extends IService<TUser> {
-}
