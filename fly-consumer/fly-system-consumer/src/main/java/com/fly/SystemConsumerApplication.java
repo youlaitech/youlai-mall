69fa01b82941e9590ac2b7406da8b4b14c0fec66
XCZTH <1490493387@qq.com>
2020-03-02 20:12:59 +0800

    配置修改

--- fly-consumer/fly-system-consumer/src/main/java/com/fly/SystemConsumerApplication.java
+++ fly-consumer/fly-system-consumer/src/main/java/com/fly/SystemConsumerApplication.java
@@ -2,13 +2,14 @@ package com.fly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by XianRui on 2020-03-02 18:38
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 @EnableFeignClients
 public class SystemConsumerApplication {
