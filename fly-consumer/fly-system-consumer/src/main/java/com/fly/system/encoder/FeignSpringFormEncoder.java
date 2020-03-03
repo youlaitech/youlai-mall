decf5e95d638c69c9170ff396c83589ba86bf428
fly <1490493387@qq.com>
2020-03-04 00:25:07 +0800

    feign上传文件bug查找

---
+++ fly-consumer/fly-system-consumer/src/main/java/com/fly/system/encoder/FeignSpringFormEncoder.java
@@ -0,0 +1,61 @@
+package com.fly.system.encoder;
+
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import feign.codec.Encoder;
+import feign.form.FormEncoder;
+import feign.form.MultipartFormContentProcessor;
+import feign.form.spring.SpringManyMultipartFilesWriter;
+import feign.form.spring.SpringSingleMultipartFileWriter;
+import lombok.val;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.lang.reflect.Type;
+
+import static feign.form.ContentType.MULTIPART;
+import static java.util.Collections.singletonMap;
+
+public class FeignSpringFormEncoder extends FormEncoder {
+    /**
+     * Constructor with the default Feign's encoder as a delegate.
+     */
+    public FeignSpringFormEncoder() {
+        this(new Encoder.Default());
+    }
+
+
+    /**
+     * Constructor with specified delegate encoder.
+     *
+     * @param delegate delegate encoder, if this encoder couldn't encode object.
+     */
+    public FeignSpringFormEncoder(Encoder delegate) {
+        super(delegate);
+
+        val processor = (MultipartFormContentProcessor) getContentProcessor(MULTIPART);
+        processor.addWriter(new SpringSingleMultipartFileWriter());
+        processor.addWriter(new SpringManyMultipartFilesWriter());
+    }
+
+
+    @Override
+    public void encode(Object object, Type bodyType, RequestTemplate template) throws EncodeException {
+        if (bodyType.equals(MultipartFile.class)) {
+            template.header("Content-Type","multipart/form-data");
+            val file = (MultipartFile) object;
+            val data = singletonMap(file.getName(), object);
+            super.encode(data, MAP_STRING_WILDCARD, template);
+            return;
+        } else if (bodyType.equals(MultipartFile[].class)) {
+            template.header("Content-Type","multipart/form-data");
+            val file = (MultipartFile[]) object;
+            if(file != null) {
+                val data = singletonMap(file.length == 0 ? "" : file[0].getName(), object);
+                super.encode(data, MAP_STRING_WILDCARD, template);
+                return;
+            }
+        }
+        super.encode(object, bodyType, template);
+    }
+
+}
