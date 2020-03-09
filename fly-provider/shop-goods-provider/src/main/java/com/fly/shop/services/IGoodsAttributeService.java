3fcd214434bd686126ed92811ce864efcaff3a39
fly <1490493387@qq.com>
2020-03-10 00:21:51 +0800

    完善商品类型的业务代码

---
+++ fly-provider/shop-goods-provider/src/main/java/com/fly/shop/services/IGoodsAttributeService.java
@@ -0,0 +1,7 @@
+package com.fly.shop.services;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.fly.shop.pojo.entity.GoodsAttribute;
+
+public interface IGoodsAttributeService extends IService<GoodsAttribute> {
+}
