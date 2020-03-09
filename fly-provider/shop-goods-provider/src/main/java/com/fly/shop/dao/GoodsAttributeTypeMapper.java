93f75e49e9a8e98c369b264b84fbc067b2b1ccf0
XCZTH <1490493387@qq.com>
2020-03-09 21:34:42 +0800

    商品类型临时提交

---
+++ fly-provider/shop-goods-provider/src/main/java/com/fly/shop/dao/GoodsAttributeTypeMapper.java
@@ -0,0 +1,23 @@
+package com.fly.shop.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fly.shop.pojo.entity.GoodsAttributeType;
+import com.fly.shop.pojo.entity.GoodsCategory;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface GoodsAttributeTypeMapper extends BaseMapper<GoodsAttributeType> {
+
+
+    @Select("<script>" +
+            "   select * from  goods_category " +
+            "   where 1=1 " +
+            "   <if test ='goodsCategory.categoryName!=null and goodsCategory.categoryName.trim() neq \"\"' >" +
+            "       and category_name like concat('%'，#{goodsCategory.categoryName}，'%') " +
+            "   </if> " +
+            "   order by sort asc " +
+            "</script>" )
+    List<GoodsCategory> page(Page<GoodsCategory> page, GoodsCategory goodsCategory);
+}
