eab3a499b07249f86c371cfe7da433c08f88926c
Xbcmz <1570435771@qq.com>
2020-03-21 23:09:02 +0800

    商品列表

--- fly-provider/shop-goods-provider/src/main/java/com/fly/shop/controller/GoodsController.java
+++ fly-provider/shop-goods-provider/src/main/java/com/fly/shop/controller/GoodsController.java
@@ -1,12 +1,12 @@
 package com.fly.shop.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fly.common.core.domain.Result;
 import com.fly.shop.pojo.dto.GoodsDTO;
+import com.fly.shop.pojo.entity.Goods;
 import com.fly.shop.services.IGoodsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -16,12 +16,19 @@ import javax.annotation.Resource;
  * @create: 2020-03-13 17:04
  **/
 @RestController
-@RequestMapping("/add")
+@RequestMapping
 public class GoodsController {
     @Resource
     private IGoodsService iGoodsService;
 
-    @PostMapping
+    @GetMapping("/pageNum/{pageNum}/pageSize/{pageSize}")
+    public Result<Page<Goods>> page(@PathVariable Integer pageNum, @PathVariable Integer pageSize, Goods goods) {
+        Page<Goods> page = new Page<>(pageNum, pageSize);
+        Page<Goods> data = (Page) iGoodsService.page(page, new LambdaQueryWrapper<Goods>());
+        return Result.success(data);
+    }
+
+    @PostMapping("/add")
     public Result add(@RequestBody GoodsDTO goodsDto) {
         boolean status = iGoodsService.add(goodsDto);
         return Result.status(status);
