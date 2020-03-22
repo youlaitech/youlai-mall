8dde2eebc05950d6c8ada933bf8f7435ab6cdac2
Xbcmz <1570435771@qq.com>
2020-03-22 08:40:18 +0800

    商品修改

--- fly-provider/shop-goods-provider/src/main/java/com/fly/shop/dao/GoodsMapper.java
+++ fly-provider/shop-goods-provider/src/main/java/com/fly/shop/dao/GoodsMapper.java
@@ -2,6 +2,15 @@ package com.fly.shop.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fly.shop.pojo.entity.Goods;
+import org.apache.ibatis.annotations.Update;
 
 public interface GoodsMapper extends BaseMapper<Goods> {
+    @Update("UPDATE goods SET publish_status = #{publishStatus} WHERE id = #{id}")
+    void updatePublishStatus(Goods goods);
+    @Update("UPDATE goods SET new_status = #{newStatus} WHERE id = #{id}")
+    void updateNewStatus(Goods goods);
+    @Update("UPDATE goods SET recommend_status = #{recommendStatus} WHERE id = #{id}")
+    void updateRecommendStatus(Goods goods);
+    @Update("UPDATE goods SET delete_status = #{deleteStatus} WHERE id = #{id}")
+    void updateDeleteStatus(Goods goods);
 }
