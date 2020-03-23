7e65bace6f5563758e5a3136841536b27ec80ff5
XCZTH <1490493387@qq.com>
2020-03-23 13:48:50 +0800

    完善退货原因和订单设置

--- fly-provider/shop-order-provider/src/main/java/com/fly4j/shop/order/config/ResourceServerConfig.java
+++ fly-provider/shop-order-provider/src/main/java/com/fly4j/shop/order/config/ResourceServerConfig.java
@@ -33,9 +33,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         "/v2/api-docs",
                         "/doc.html",
                         "/swagger-ui.html",
-                        "/swagger-resources/**",
-                        "/system/**",
-                        "/brands/**").permitAll()
+                        "/swagger-resources/**"
+                ).permitAll()
                 .antMatchers(HttpMethod.OPTIONS,"/oauth/token").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
