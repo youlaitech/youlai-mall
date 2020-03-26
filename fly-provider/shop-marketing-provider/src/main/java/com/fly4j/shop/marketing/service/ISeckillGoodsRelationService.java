f27ae37dc839bdad275f39c83edb3eab665916bf
XCZTH <1490493387@qq.com>
2020-03-26 23:08:11 +0800

    添加营销模块

---
+++ fly-provider/shop-marketing-provider/src/main/java/com/fly4j/shop/marketing/service/ISeckillGoodsRelationService.java
@@ -0,0 +1,8 @@
+package com.fly4j.shop.marketing.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.fly4j.shop.marketing.pojo.entity.SeckillGoodsRelation;
+
+public interface ISeckillGoodsRelationService extends IService<SeckillGoodsRelation> {
+
+}
---
+++ fly-provider/shop-marketing-provider/src/main/java/com/fly4j/shop/marketing/service/impl/SeckillRecordServiceImpl.java
@@ -0,0 +1,15 @@
+package com.fly4j.shop.marketing.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fly4j.shop.marketing.mapper.SeckillMapper;
+import com.fly4j.shop.marketing.mapper.SeckillRecordMapper;
+import com.fly4j.shop.marketing.pojo.entity.Seckill;
+import com.fly4j.shop.marketing.pojo.entity.SeckillRecord;
+import com.fly4j.shop.marketing.service.ISeckillRecordService;
+import com.fly4j.shop.marketing.service.ISeckillService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeckillRecordServiceImpl extends ServiceImpl<SeckillRecordMapper, SeckillRecord> implements ISeckillRecordService {
+
+}
