0e5a5fd9fa093d0d2ce43c6f424b9cc5e4860cb8
XCZTH <1490493387@qq.com>
2020-03-31 18:55:49 +0800

    广告页提交

---
+++ shop-api/shop-marketing/src/main/java/com/fly4j/shop/marketing/mapper/HomeAdvertisementMapper.java
@@ -0,0 +1,8 @@
+package com.fly4j.shop.marketing.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fly4j.shop.marketing.pojo.entity.HomeAdvertisementEntity;
+
+public interface HomeAdvertisementMapper extends BaseMapper<HomeAdvertisementEntity> {
+
+}
--- shop-api/shop-marketing/src/main/java/com/fly4j/shop/marketing/pojo/entity/HomeAdvertisementEntity.java
+++ shop-api/shop-marketing/src/main/java/com/fly4j/shop/marketing/pojo/entity/HomeAdvertisementEntity.java
@@ -1,8 +1,14 @@
 package com.fly4j.shop.marketing.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fly4j.common.core.domain.BaseEntity;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.util.Date;
 
 /**
  * 系统首页广告实体
@@ -13,10 +19,29 @@ import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
+@TableName("m_home_advertisement")
 public class HomeAdvertisementEntity extends BaseEntity {
 
+    @TableId(type = IdType.AUTO)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long id;
 
+    private String name;
 
+    private Integer type;
 
+    private String imageUrl;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date startTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date endTime;
+
+    private Integer status;
+
+    private String linkUrl;
+
+    private String remark;
 
 }
