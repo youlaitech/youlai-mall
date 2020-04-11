4ef8117d47cfe1bf39ed68a89104317ac3848a3d
fly <1490493387@qq.com>
2020-04-11 12:58:11 +0800

    认证中心配置修改

--- yshop-config/src/main/java/com/fly4j/yshop/config/ConfigApplication.java
+++ yshop-config/src/main/java/com/fly4j/yshop/config/ConfigApplication.java
@@ -1,6 +1,5 @@
 package com.fly4j.yshop.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
