f5c20ada71098c843d4b71bc9d14617e33c72244
XCZTH <1490493387@qq.com>
2020-04-17 09:48:01 +0800

    pms的app调用feign客户端添加

---
+++ yshop-pms/pms-api/src/main/java/com/fly4j/yshop/pms/feign/PmsAppFeign.java
@@ -0,0 +1,16 @@
+package com.fly4j.yshop.pms.feign;
+
+import com.baomidou.mybatisplus.extension.api.R;
+import com.fly4j.yshop.pms.feign.factory.PmsAppFeignFallbackFactory;
+import com.fly4j.yshop.pms.feign.factory.PmsFeignFallbackFactory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "yshop-pms", fallbackFactory = PmsAppFeignFallbackFactory.class)
+public interface PmsAppFeign {
+
+    @GetMapping(value = "/spus/{id}")
+    R getSpuById(@PathVariable Long id);
+
+}
