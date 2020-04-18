70430ac11eee2c935416528a5e17da385c366f6d
Xbcmz <1570435771@qq.com>
2020-04-18 23:06:01 +0800

    订单提交

--- yshop-pms/pms-api/src/main/java/com/fly4j/yshop/pms/feign/factory/PmsAppFeignFallbackFactory.java
+++ yshop-pms/pms-api/src/main/java/com/fly4j/yshop/pms/feign/factory/PmsAppFeignFallbackFactory.java
@@ -2,7 +2,10 @@ package com.fly4j.yshop.pms.feign.factory;
 
 import com.baomidou.mybatisplus.extension.api.R;
 import com.fly4j.yshop.pms.feign.PmsAppFeign;
+import com.fly4j.yshop.pms.pojo.vo.SkuLockVO;
 import feign.hystrix.FallbackFactory;
+
+import java.util.List;
 
 public class PmsAppFeignFallbackFactory implements FallbackFactory<PmsAppFeign> {
 
@@ -12,6 +15,11 @@ public class PmsAppFeignFallbackFactory implements FallbackFactory<PmsAppFeign> 
         return new PmsAppFeign() {
             @Override
             public R getSkuById(Long id) {
+                return null;
+            }
+
+            @Override
+            public R<SkuLockVO> checkAndLockStock(List<SkuLockVO> skuLockVOS) {
                 return null;
             }
         };
--- yshop-pms/pms-service/src/main/java/com/fly4j/yshop/pms/controller/app/SkuController.java
+++ yshop-pms/pms-service/src/main/java/com/fly4j/yshop/pms/controller/app/SkuController.java
@@ -3,14 +3,14 @@ package com.fly4j.yshop.pms.controller.app;
 import com.baomidou.mybatisplus.extension.api.R;
 import com.fly4j.common.core.controller.BaseController;
 import com.fly4j.yshop.pms.pojo.entity.PmsSku;
+import com.fly4j.yshop.pms.pojo.vo.SkuLockVO;
 import com.fly4j.yshop.pms.service.IPmsSkuService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api.app/v1/skus")
@@ -23,5 +23,14 @@ public class SkuController extends BaseController {
     public R get(@PathVariable Long id) {
         PmsSku sku = iPmsSkuService.getById(id);
         return R.ok(sku);
+    }
+
+    @PostMapping("/lock")
+    public R checkAndLockStock(@RequestBody List<SkuLockVO> skuLockVOS) {
+        String msg = iPmsSkuService.checkAndLockStock(skuLockVOS);
+        if (StringUtils.isNotBlank(msg)) {
+            return R.failed(msg);
+        }
+        return R.ok(null);
     }
 }
