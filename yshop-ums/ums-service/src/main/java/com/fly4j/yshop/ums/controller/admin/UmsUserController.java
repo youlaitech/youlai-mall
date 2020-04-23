24e5f357d2e4cc088c8257c544d236f3c47111f8
fly <1490493387@qq.com>
2020-04-23 21:34:29 +0800

    地址和优惠券接口编写

--- yshop-ums/ums-service/src/main/java/com/fly4j/yshop/ums/controller/admin/UmsUserController.java
+++ yshop-ums/ums-service/src/main/java/com/fly4j/yshop/ums/controller/admin/UmsUserController.java
@@ -1,4 +1,4 @@
-package com.fly4j.yshop.ums.controller;
+package com.fly4j.yshop.ums.controller.admin;
 
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
