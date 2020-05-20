3d26641c4a5a111308766367225743ff672555c3
Mr <34190452+haoxs1995@users.noreply.github.com>
2020-05-20 20:19:32 +0800

    Update OmsOrderServiceImpl.java

--- yshop-service/yshop-oms/src/main/java/com/fly4j/yshop/oms/service/impl/OmsOrderServiceImpl.java
+++ yshop-service/yshop-oms/src/main/java/com/fly4j/yshop/oms/service/impl/OmsOrderServiceImpl.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author fly2021【xianrui0365@163.com】
+ * @author Mr.
  * @date 2020-04-13 15:02
  **/
 @Service
@@ -53,11 +53,11 @@ public class OmsOrderServiceImpl extends ServiceImpl<OmsOrderMapper, OmsOrder> i
          *  1. 验证令牌，是否重复提交
          */
         String orderToken = orderDTO.getOrder_token();
-//        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
-//        Long flag = stringRedisTemplate.execute(new DefaultRedisScript<>(script, Long.class), Arrays.asList(TOKEN_PREFIX + orderToken), orderToken);
-//        if (flag == 0L) {
-//            return R.failed("订单不可重复提交");
-//        }
+       String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+       Long flag = stringRedisTemplate.execute(new DefaultRedisScript<>(script, Long.class), Arrays.asList(TOKEN_PREFIX + orderToken), orderToken);
+       if (flag == 0L) {
+           return R.failed("订单不可重复提交");
+       }
 
         /**
          * 2. 验证价格,防止用户在订单页面停留太久，导致的价格不一致
@@ -68,20 +68,20 @@ public class OmsOrderServiceImpl extends ServiceImpl<OmsOrderMapper, OmsOrder> i
             return R.failed("请选择商品再提交");
         }
 
-//        BigDecimal currentTotalPrice = orderItems.stream().map(item -> {
-//            // 查询数据库中商品的价格
-//            R<PmsSku> pmsSku = pmsAppFeign.getSkuById(item.getSku_id());
-//            PmsSku skuInfo = pmsSku.getData();
-//            if (skuInfo == null) {
-//                return new BigDecimal("0");
-//            }
-//            return skuInfo.getPrice().multiply(new BigDecimal(item.getSku_quantity()));
-//        }).reduce(new BigDecimal("0"), BigDecimal::add);
-//
-//        // 如果价格不同，直接返回
-//        if (currentTotalPrice.compareTo(orderDTO.getOrder().getTotal_amount()) != 0) {
-//            return R.failed("页面已过期,请刷新后重试");
-//        }
+       BigDecimal currentTotalPrice = orderItems.stream().map(item -> {
+           // 查询数据库中商品的价格
+           R<PmsSku> pmsSku = pmsAppFeign.getSkuById(item.getSku_id());
+           PmsSku skuInfo = pmsSku.getData();
+           if (skuInfo == null) {
+               return new BigDecimal("0");
+           }
+           return skuInfo.getPrice().multiply(new BigDecimal(item.getSku_quantity()));
+       }).reduce(new BigDecimal("0"), BigDecimal::add);
+
+       // 如果价格不同，直接返回
+       if (currentTotalPrice.compareTo(orderDTO.getOrder().getTotal_amount()) != 0) {
+           return R.failed("页面已过期,请刷新后重试");
+       }
 
 
         /**
