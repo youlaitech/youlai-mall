787bde7c6580672af7919f0a350acc611c36954e
haoxianrui@ <1490493387@qq.com>
2020-06-29 23:56:35 +0800

    移除youlai-auth认证模块

--- youlai-auth/src/main/java/com/youlai/mall/auth/service/ISysUserService.java
+++
@@ -1,8 +0,0 @@
-package com.youlai.mall.auth.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.youlai.mall.auth.entity.SysUser;
-
-public interface ISysUserService extends IService<SysUser> {
-    SysUser getByUserName(String username);
-}
