03addfc1b938f76cb2831f32a6903b528d6e400a
haoxianrui@ <1490493387@qq.com>
2020-07-06 01:15:06 +0800

    完成minio上传文件接口

---
+++ youlai-service/src/main/java/com/youlai/service/oauth/mapper/SysUserMapper.java
@@ -0,0 +1,11 @@
+package com.youlai.service.oauth.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.youlai.service.oauth.entity.SysUser;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
