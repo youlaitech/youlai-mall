450da8d3af7b4464d0ef730ec6522b0e29aa9309
XCZTH <1490493387@qq.com>
2020-09-01 20:00:08 +0800

    网关整个nacos和oauth2认证资源授权测试

--- youlai-gateway/src/main/java/com/youlai/GatewayApplication.java
+++ youlai-gateway/src/main/java/com/youlai/GatewayApplication.java
@@ -1,5 +1,4 @@
-package com.youlai.gateway;
-
+package com.youlai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
---
+++ youlai-gateway/src/main/java/com/youlai/gateway/config/ResourceServerConfig.java
@@ -0,0 +1,79 @@
+package com.youlai.gateway.config;
+
+
+import cn.hutool.core.util.ArrayUtil;
+import com.youlai.common.auth.constant.AuthConstant;
+import com.youlai.gateway.auth.AuthorizationManager;
+import com.youlai.gateway.component.AuthServerAuthenticationEntryPoint;
+import com.youlai.gateway.component.ResourceServerAccessDeniedHandler;
+import com.youlai.gateway.filter.WhiteUrlsRemoveJwtFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * 资源服务器配置
+ */
+@AllArgsConstructor
+@Configuration
+@EnableWebFluxSecurity
+public class ResourceServerConfig {
+
+    private AuthorizationManager authorizationManager;
+    private ResourceServerAccessDeniedHandler resourceServerAccessDeniedHandler;
+    private AuthServerAuthenticationEntryPoint authServerAuthenticationEntryPoint;
+    private WhiteUrlsConfig whiteUrlsConfig;
+    private WhiteUrlsRemoveJwtFilter whiteUrlsRemoveJwtFilter;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+        // 自定义处理JWT请求头过期或签名错误的结果
+        http.oauth2ResourceServer().authenticationEntryPoint(authServerAuthenticationEntryPoint);
+        // 对白名单路径，直接移除JWT请求头
+        http.addFilterBefore(whiteUrlsRemoveJwtFilter, SecurityWebFiltersOrder.AUTHENTICATION);
+        http.authorizeExchange()
+                .pathMatchers(ArrayUtil.toArray(whiteUrlsConfig.getUrls(),String.class)).permitAll()
+                .anyExchange().access(authorizationManager)
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(resourceServerAccessDeniedHandler) // 处理未授权
+                .authenticationEntryPoint(authServerAuthenticationEntryPoint) //处理未认证
+                .and().csrf().disable();
+
+        return http.build();
+    }
+
+    /**
+     * https://blog.csdn.net/qq_24230139/article/details/105091273
+     * ServerHttpSecurity没有将jwt中authorities的负载部分当做Authentication
+     * 需要把jwt的Claim中的authorities加入
+     * 方案：重新定义ReactiveAuthenticationManager权限管理器，默认转换器JwtGrantedAuthoritiesConverter
+     *
+     * @return
+     */
+    @Bean
+    public Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(AuthConstant.AUTHORITY_PREFIX);
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(AuthConstant.AUTHORITY_CLAIM_NAME);
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
+
+}
