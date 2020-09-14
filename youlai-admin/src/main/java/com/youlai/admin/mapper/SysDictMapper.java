561af63ec8c71f014a4cec0141f5e88e42de4783
haoxr <1490493387@qq.com>
2020-09-14 18:06:54 +0800

    添加菜单路由接口和单元测试

--- youlai-admin/src/main/java/com/youlai/admin/mapper/SysDictMapper.java
+++ youlai-admin/src/main/java/com/youlai/admin/mapper/SysDictMapper.java
@@ -13,8 +13,12 @@ public interface SysDictMapper extends BaseMapper<SysDict> {
 
     @Select("<script>" +
             " select a.*,b.name as type_name from sys_dict a left join sys_dict_type b on a.type_code=b.code " +
-            " <if test='dict.name!=null and dict.name.trim() neq \"\"'>" +
+            " where 1=1 " +
+            " <if test='dict.name != null and dict.name.trim() neq \"\"'>" +
             "   and a.name like concat('%',#{dict.name},'%')" +
+            " </if>" +
+            " <if test='dict.typeCode !=null and dict.typeCode.trim() neq \"\"'>" +
+            "   and a.type_code = #{dict.typeCode}" +
             " </if>" +
             "</script>")
     List<SysDict> list(Page<SysDict> page, SysDict dict);
