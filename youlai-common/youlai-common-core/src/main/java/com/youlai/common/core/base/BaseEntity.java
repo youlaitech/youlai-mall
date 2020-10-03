fb204b804f075fe365ee699d99ab500280418927
haoxr <1490493387@qq.com>
2020-10-03 16:38:11 +0800

    fix:修复依赖错误

--- youlai-common/youlai-common-core/src/main/java/com/youlai/common/core/base/BaseEntity.java
+++ youlai-common/youlai-common-core/src/main/java/com/youlai/common/core/base/BaseEntity.java
@@ -17,19 +17,19 @@ public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
 
-     @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "创建人",example = "admin")
+    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建人", example = "admin")
     private String createBy;
 
-     @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "创建时间",example = "2020-01-01 12:00:00")
+    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建时间", example = "2020-01-01 12:00:00")
     private Date createTime;
 
-     @TableField(fill = FieldFill.INSERT_UPDATE)
-    @ApiModelProperty(value = "修改人",example = "admin")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty(value = "修改人", example = "admin")
     private String updateBy;
 
-     @TableField(fill = FieldFill.INSERT_UPDATE)
-    @ApiModelProperty(value = "创建时间",example = "2020-01-01 12:00:00")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty(value = "创建时间", example = "2020-01-01 12:00:00")
     private Date updateTime;
 }
---
+++ youlai-mall-sms/youlai-mall-sms-api/src/main/java/com/youlai/mall/sms/api/entity/SmsAdvert.java
@@ -0,0 +1,35 @@
+package com.youlai.mall.sms.api.entity;
+
+import cn.hutool.core.date.DateTime;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.youlai.common.core.base.BaseEntity;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SmsAdvert extends BaseEntity {
+
+    @TableId(type= IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String pic;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Integer status;
+
+    private Integer sort;
+
+    private  String url;
+
+    private Integer clickCount;
+
+    private String remark;
+
+}
