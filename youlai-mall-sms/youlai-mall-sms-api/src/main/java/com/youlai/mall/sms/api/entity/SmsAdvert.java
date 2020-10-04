659d1b8796d4ed5332bb6fdb3129c7d3ba73e81d
haoxr <1490493387@qq.com>
2020-10-04 23:33:37 +0800

    feat

--- youlai-mall-sms/youlai-mall-sms-api/src/main/java/com/youlai/mall/sms/api/entity/SmsAdvert.java
+++ youlai-mall-sms/youlai-mall-sms-api/src/main/java/com/youlai/mall/sms/api/entity/SmsAdvert.java
@@ -1,6 +1,5 @@
 package com.youlai.mall.sms.api.entity;
 
-import cn.hutool.core.date.DateTime;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.youlai.common.core.base.BaseEntity;
---
+++ youlai-mall-sms/youlai-mall-sms-biz/src/main/java/com/youlai/mall/sms/SmsApplication.java
@@ -0,0 +1,9 @@
+package com.youlai.mall.sms;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.youlai.admin.mapper")
+@EnableSwagger2
+public class SmsApplication {
+}
