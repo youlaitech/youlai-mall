191ea795630f8c326ae3df7eb931ea298fd4886f
haoxr <1490493387@qq.com>
2020-10-05 23:54:51 +0800

    feat:添加oms、pms、ums模块

---
+++ ylmall-sms/ylmall-sms-api/src/main/java/com/ylmall/sms/api/entity/SmsAdvert.java
@@ -0,0 +1,34 @@
+package com.ylmall.sms.api.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.youlai.common.core.base.BaseEntity;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SmsAdvert extends BaseEntity {
+
+    @TableId(type= IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String pic;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Integer status;
+
+    private Integer sort;
+
+    private  String url;
+
+    private Integer clickCount;
+
+    private String remark;
+
+}
---
+++ ylmall-sms/ylmall-sms-biz/src/main/java/com/ylmall/sms/SmsApplication.java
@@ -0,0 +1,9 @@
+package com.ylmall.sms;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.youlai.admin.mapper")
+@EnableSwagger2
+public class SmsApplication {
+}
