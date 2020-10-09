96f4080b8ab083f82056e864ec8efde9ec97719d
haoxr <1490493387@qq.com>
2020-10-09 20:09:30 +0800

    feat:整合oauth2微信登录

---
+++ mall-ums/mall-ums-api/src/main/java/com/youlai/mall/ums/api/dto/MemberDTO.java
@@ -0,0 +1,14 @@
+package com.youlai.mall.ums.api.dto;
+
+import lombok.Data;
+
+
+@Data
+public class MemberDTO {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private Integer status;
+    private String clientId;
+}
