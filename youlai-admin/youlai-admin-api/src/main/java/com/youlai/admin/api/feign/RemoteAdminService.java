252db0178f444fc506185a552ae530c162e46331
haoxr <1490493387@qq.com>
2020-10-12 20:15:29 +0800

    fix:MemberDTO引用错误问题修复

--- youlai-admin/youlai-admin-api/src/main/java/com/youlai/admin/api/feign/RemoteAdminService.java
+++ youlai-admin/youlai-admin-api/src/main/java/com/youlai/admin/api/feign/RemoteAdminService.java
@@ -1,6 +1,7 @@
 package com.youlai.admin.api.feign;
 
 import com.youlai.admin.api.dto.UserDTO;
+import com.youlai.common.core.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,5 +10,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface RemoteAdminService {
 
     @GetMapping("/users/user/{username}")
-    UserDTO loadUserByUsername(@PathVariable String username);
+    Result<UserDTO> loadUserByUsername(@PathVariable String username);
 }
