2cebc430983eb7adb0c9d7f5c45f8f5cfb34faa1
haoxr <1490493387@qq.com>
2020-10-22 22:37:00 +0800

    refactor:移除自定义认证异常

--- youlai-auth/src/main/java/com/youlai/AuthApplication.java
+++ youlai-auth/src/main/java/com/youlai/AuthApplication.java
@@ -1,7 +1,6 @@
 package com.youlai;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
