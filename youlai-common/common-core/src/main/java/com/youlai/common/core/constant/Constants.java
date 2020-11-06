6631090bc2a85c00a61e35bebdc69f696b15ec6f
haoxr <1490493387@qq.com>
2020-11-06 18:04:35 +0800

    refactor:重构菜单接口

--- youlai-common/common-core/src/main/java/com/youlai/common/core/constant/Constants.java
+++ youlai-common/common-core/src/main/java/com/youlai/common/core/constant/Constants.java
@@ -2,9 +2,8 @@ package com.youlai.common.core.constant;
 
 public interface Constants {
 
-    /**
-     * 正常状态值
-     */
     Integer STATUS_NORMAL_VALUE = 1;
+
+    Integer VISIBLE_SHOW_VALUE = 1;
 
 }
