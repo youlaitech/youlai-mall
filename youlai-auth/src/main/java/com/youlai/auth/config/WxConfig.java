280799371bd2eeccadb6e8595a27c8903c79cbb1
haoxr <1490493387@qq.com>
2020-11-17 09:30:01 +0800

    refactor:项目结构优化

--- youlai-auth/src/main/java/com/youlai/auth/config/WxConfig.java
+++ youlai-auth/src/main/java/com/youlai/auth/config/WxConfig.java
@@ -4,20 +4,24 @@ import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
 import cn.binarywang.wx.miniapp.config.WxMaConfig;
 import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AllArgsConstructor
 public class WxConfig {
-    private WxProperties properties;
+
+    @Value("${wx.miniapp.appid}")
+    private String appid;
+
+    @Value("${wx.miniapp.secret}")
+    private String secret;
 
     @Bean
     public WxMaConfig wxMaConfig() {
         WxMaDefaultConfigImpl config = new WxMaDefaultConfigImpl();
-        config.setAppid(properties.getAppid());
-        config.setSecret(properties.getSecret());
+        config.setAppid(appid);
+        config.setSecret(secret);
         return config;
     }
 
