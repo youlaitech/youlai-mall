095427cace3165365cc0a92635990f3a8f37b1a7
haoxr <1490493387@qq.com>
2020-11-19 17:08:06 +0800

    feat:添加自定义认证异常

--- youlai-auth/src/main/java/com/youlai/auth/exception/AuthExceptionHandler.java
+++ youlai-auth/src/main/java/com/youlai/auth/exception/AuthExceptionHandler.java
@@ -3,6 +3,8 @@ package com.youlai.auth.exception;
 import com.youlai.common.core.result.Result;
 import com.youlai.common.core.result.ResultCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,8 +13,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class AuthExceptionHandler {
 
+    /**
+     * 用户名和密码异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(InvalidGrantException.class)
+    public Result handleInvalidGrantException(InvalidGrantException e){
+        return Result.error(e.getMessage());
+    }
+
+
+    /**
+     * 账户异常(禁用、锁定、过期)
+     * @param e
+     * @return
+     */
+    @ExceptionHandler({InternalAuthenticationServiceException.class})
+    public Result handleInternalAuthenticationServiceException(InternalAuthenticationServiceException e){
+        return Result.error(e.getMessage());
+    }
+
+
     @ExceptionHandler(InvalidTokenException.class)
-    public Result handleBizException(InvalidTokenException e) {
+    public Result handleInvalidTokenException(InvalidTokenException e) {
         return Result.custom(ResultCode.TOKEN_INVALID_OR_EXPIRED);
     }
 }
