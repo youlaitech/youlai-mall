f32944ab43e484d87a6bc6c681cea1fb7f7eea0f
haoxr <1490493387@qq.com>
2020-11-20 20:26:14 +0800

    feat:jwt续期实现

--- youlai-auth/src/main/java/com/youlai/auth/exception/AuthExceptionHandler.java
+++ youlai-auth/src/main/java/com/youlai/auth/exception/AuthExceptionHandler.java
@@ -1,30 +1,15 @@
 package com.youlai.auth.exception;
 
 import com.youlai.common.core.result.Result;
-import com.youlai.common.core.result.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class AuthExceptionHandler {
-
-
-    /**
-     * 客户端信息错误
-     *
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public Result handleUsernameNotFoundException(UsernameNotFoundException e) {
-        return Result.error(e.getMessage());
-    }
 
     /**
      * 用户名和密码异常
@@ -49,14 +34,4 @@ public class AuthExceptionHandler {
         return Result.error(e.getMessage());
     }
 
-
-    @ExceptionHandler(InvalidTokenException.class)
-    public Result handleInvalidTokenException(InvalidTokenException e) {
-        return Result.custom(ResultCode.TOKEN_INVALID_OR_EXPIRED);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public Result handleException(Exception e) {
-        return Result.error(e.getMessage());
-    }
 }
