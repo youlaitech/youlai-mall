4ef2746a8144aea7b727d968736ada333b9d6b5d
haoxr <1490493387@qq.com>
2020-12-16 19:34:33 +0800

    :添加系统枚举

--- youlai-common/common-web/src/main/java/com/youlai/common/web/util/WebUtils.java
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/util/WebUtils.java
@@ -1,9 +1,11 @@
 package com.youlai.common.web.util;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.youlai.common.core.constant.AuthConstants;
+import com.youlai.common.core.constant.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -14,9 +16,13 @@ import javax.servlet.http.HttpServletRequest;
 public class WebUtils extends org.springframework.web.util.WebUtils {
 
 
-    public static JSONObject getJwtPayload(){
+    public static HttpServletRequest getHttpServletRequest() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String jwtPayload = request.getHeader(AuthConstants.JWT_PAYLOAD_KEY);
+        return request;
+    }
+
+    public static JSONObject getJwtPayload() {
+        String jwtPayload = getHttpServletRequest().getHeader(AuthConstants.JWT_PAYLOAD_KEY);
         JSONObject jsonObject = JSONUtil.parseObj(jwtPayload);
         return jsonObject;
     }
@@ -29,6 +35,14 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
     public static String getClientId() {
         String clientId = getJwtPayload().getStr(AuthConstants.JWT_CLIENT_ID_KEY);
         return clientId;
+    }
+
+    public static Integer getSystemType() {
+        String systemType = getHttpServletRequest().getHeader(Constants.SYSTEM_TYPE_KEY);
+        if (StrUtil.isNotBlank(systemType)) {
+            return Integer.valueOf(systemType);
+        }
+        return null;
     }
 
 }
