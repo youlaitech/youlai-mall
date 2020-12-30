693594ca2ac6549a763a87a209b7e3f180e3e472
dongtiandexue <huawei_code@163.com>
2020-12-30 23:40:37 +0800

    feat:购物车数据模型创建

---
+++ mall-oms/oms-biz/src/main/java/com/youlai/mall/oms/controller/app/CartController.java
@@ -0,0 +1,25 @@
+package com.youlai.mall.oms.controller.app;
+
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "购物车接口")
+@RestController
+@RequestMapping("oms/cart")
+@Slf4j
+@AllArgsConstructor
+public class CartController {
+
+    // 添加购物车
+
+    // 删除购物车
+
+    // 批量删除购物车
+
+    // 修改购物车
+
+    // 查询购物车
+}
