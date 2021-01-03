17865c9454af61096e39278adbbd6b453b65b786
haoxr <1490493387@qq.com>
2021-01-03 11:02:31 +0800

    fix:移除jackson序列化导致程序报错问题

--- youlai-common/common-web/src/main/java/com/youlai/common/web/vo/CascaderVO.java
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/vo/CascaderVO.java
@@ -1,26 +1,18 @@
 package com.youlai.common.web.vo;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
-
-import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Accessors(chain = true)
-@ApiModel
 public class CascaderVO {
 
-    @ApiModelProperty(name="节点value")
     private String value;
 
-    @ApiModelProperty(name="节点label")
     private String label;
 
-    @ApiModelProperty(name="子节点")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<CascaderVO> children;
 }
