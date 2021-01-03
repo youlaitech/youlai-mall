269401429e78a60b53a8e373520e5e8859e0294f
haoxr <1490493387@qq.com>
2021-01-03 11:02:31 +0800

    fix:移除jackson序列化导致程序报错问题

--- youlai-common/common-redis/src/main/java/com/youlai/common/redis/RedisConfig.java
+++ youlai-common/common-redis/src/main/java/com/youlai/common/redis/RedisConfig.java
@@ -1,63 +1,24 @@
 package com.youlai.common.redis;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
-//        redisTemplate.setConnectionFactory(factory);
-//        redisTemplate.afterPropertiesSet();
-//        return redisTemplate;
-
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
         redisTemplate.setConnectionFactory(factory);
-        // 用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        redisTemplate.setValueSerializer(serializer());
-
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        // 使用StringRedisSerializer来序列化和反序列化redis的key值
-        redisTemplate.setKeySerializer(stringRedisSerializer);
-
-        // hash的key也采用String的序列化方式
-        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-        // hash的value序列化方式采用jackson
-        redisTemplate.setHashValueSerializer(serializer());
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 
-
-    /**
-     * 配置Jackson2JsonRedisSerializer序列化策略
-     * */
-    private Jackson2JsonRedisSerializer<Object> serializer() {
-        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
-        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
-//        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        return jackson2JsonRedisSerializer;
-    }
 }
