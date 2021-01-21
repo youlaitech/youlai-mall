18779ce9e8bc6edc01b82654a44833b2d558448e
hxrui <1490493387@qq.com>
2021-01-21 16:45:10 +0800

    fix:移除fastjson依赖

--- mall-pms/pms-search/src/main/java/com/youlai/mall/pms/search/service/impl/PmsSearchServiceImpl.java
+++ mall-pms/pms-search/src/main/java/com/youlai/mall/pms/search/service/impl/PmsSearchServiceImpl.java
@@ -1,36 +1,18 @@
 package com.youlai.mall.pms.search.service.impl;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.youlai.mall.pms.pojo.PmsSku;
 import com.youlai.mall.pms.pojo.PmsSpu;
 import com.youlai.mall.pms.search.service.IPmsSearchService;
 import com.youlai.mall.pms.search.utils.ElasticsearchConvertUtils;
 import com.youlai.mall.pms.search.utils.ElasticsearchUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import static com.youlai.common.core.constant.EsIndexContstants.PMS_SPU;
 
