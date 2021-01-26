9a437852f179c5efd5737bfe219099994518d5e6
hxrui <1490493387@qq.com>
2021-01-26 14:03:17 +0800

    fix:修复日期不自动填充的问题

--- youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/handler/CustomMetaObjectHandler.java
+++ youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/handler/CustomMetaObjectHandler.java
@@ -1,16 +1,19 @@
 package com.youlai.common.mybatis.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
-public class MetaHandler implements MetaObjectHandler {
+@Slf4j
+public class CustomMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
+        log.info("自动填充时间字段");
         this.setFieldValByName("gmtCreate", new Date(), metaObject);
         this.setFieldValByName("gmtModified", new Date(), metaObject);
     }
