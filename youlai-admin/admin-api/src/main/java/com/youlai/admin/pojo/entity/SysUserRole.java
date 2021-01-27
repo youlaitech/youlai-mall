aff6a82dbfed36fe3ee21086e4123eff7a98369b
hxrui <1490493387@qq.com>
2021-01-27 20:17:15 +0800

    feat:菜单权限模块功能重构

---
+++ youlai-admin/admin-api/src/main/java/com/youlai/admin/pojo/entity/SysUserRole.java
@@ -0,0 +1,14 @@
+package com.youlai.admin.pojo.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class SysUserRole {
+
+    private Long userId;
+
+    private Long roleId;
+
+}
