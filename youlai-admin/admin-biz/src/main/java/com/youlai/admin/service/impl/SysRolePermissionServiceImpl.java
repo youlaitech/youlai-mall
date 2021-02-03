506f7a4cbe808e90fc9bc99285ce634a28adb2df
haoxr <1490493387@qq.com>
2021-02-03 23:59:55 +0800

    feat:添加获取角色拥有权限的接口

--- youlai-admin/admin-biz/src/main/java/com/youlai/admin/service/impl/SysRolePermissionServiceImpl.java
+++ youlai-admin/admin-biz/src/main/java/com/youlai/admin/service/impl/SysRolePermissionServiceImpl.java
@@ -6,7 +6,13 @@ import com.youlai.admin.pojo.entity.SysRolePermission;
 import com.youlai.admin.service.ISysRolePermissionService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SysRolePermissionServiceImpl extends ServiceImpl<SysRolePermissionMapper, SysRolePermission> implements ISysRolePermissionService {
 
+    @Override
+    public List<Long> listPermissionIds(Long roleId, Integer type) {
+        return this.baseMapper.listPermissionIds( roleId, type);
+    }
 }
