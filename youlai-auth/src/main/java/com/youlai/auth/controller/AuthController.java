faf4b8dd478a25bf99eaf81917969d4442c23a83
hxrui <1490493387@qq.com>
2021-02-26 20:38:57 +0800

    refactor:删除多余引入

--- youlai-auth/src/main/java/com/youlai/auth/controller/AuthController.java
+++ youlai-auth/src/main/java/com/youlai/auth/controller/AuthController.java
@@ -64,6 +64,13 @@ public class AuthController {
     ) throws HttpRequestMethodNotSupportedException {
         OAuth2AccessToken oAuth2AccessToken;
 
+        /**
+         * 获取登录认证的客户端ID
+         *
+         * 兼容两种方式获取Oauth2客户端信息（client_id、client_secret）
+         * 方式一：client_id、client_secret放在请求路径中
+         * 方式二：放在请求头（Request Headers）中的Authorization字段，且经过加密，例如 Basic Y2xpZW50OnNlY3JldA== 明文等于 client:secret
+         */
         String clientId = RequestUtils.getAuthClientId();
         switch (clientId) {
             case AuthConstants.WEAPP_CLIENT_ID:  // 微信认证
