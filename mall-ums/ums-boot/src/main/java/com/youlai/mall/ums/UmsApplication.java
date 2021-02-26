578b3936305369685338253b73936ae715fc010a
hxrui <1490493387@qq.com>
2021-02-26 20:38:57 +0800

    refactor:删除多余引入

--- mall-ums/ums-boot/src/main/java/com/youlai/mall/ums/UmsApplication.java
+++ mall-ums/ums-boot/src/main/java/com/youlai/mall/ums/UmsApplication.java
@@ -1,6 +1,5 @@
 package com.youlai.mall.ums;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
-@MapperScan("com.youlai.mall.ums.mapper")
 public class UmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(UmsApplication.class);
