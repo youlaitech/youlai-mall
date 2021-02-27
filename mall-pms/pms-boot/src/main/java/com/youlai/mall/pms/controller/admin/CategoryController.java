538da1f3e1f197962f3fa2cc68a28497550546f0
haoxr <1490493387@qq.com>
2021-02-27 23:39:25 +0800

    refactor:商品分类接口优化完善

--- mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/controller/admin/CategoryController.java
+++ mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/controller/admin/CategoryController.java
@@ -6,7 +6,9 @@ import com.youlai.common.enums.QueryModeEnum;
 import com.youlai.common.result.Result;
 import com.youlai.mall.pms.pojo.domain.PmsCategory;
 import com.youlai.mall.pms.pojo.vo.CategoryVO;
+import com.youlai.mall.pms.service.IPmsCategoryAttrService;
 import com.youlai.mall.pms.service.IPmsCategoryService;
+import com.youlai.mall.pms.service.IPmsCategorySpecService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -17,16 +19,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Api(tags = "【系统管理】商品分类")
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/api.admin/v1/categories")
 @Slf4j
 @AllArgsConstructor
 public class CategoryController {
 
     private IPmsCategoryService iPmsCategoryService;
+
+    private IPmsCategoryAttrService iPmsCategoryAttrService;
+
+    private IPmsCategorySpecService iPmsCategorySpecService;
 
     @ApiOperation(value = "列表分页", httpMethod = "GET")
     @ApiImplicitParams({
@@ -82,22 +89,27 @@ public class CategoryController {
     @ApiImplicitParam(name = "ids", value = "id集合,以英文逗号','分隔", required = true, paramType = "query", dataType = "String")
     @DeleteMapping
     public Result delete(@RequestParam String ids) {
-        iPmsCategoryService.removeByIds(Arrays.asList(ids.split(",")).stream().map(id -> Long.parseLong(id)).collect(Collectors.toList()));
+        List<String> idList = Arrays.asList(ids.split(","));
+        Optional.ofNullable(idList).ifPresent(list -> {
+            list.forEach(id -> {
+                iPmsCategoryAttrService.removeById(id);
+                iPmsCategorySpecService.removeById(id);
+            });
+            iPmsCategoryService.removeByIds(idList.stream().map(id -> Long.parseLong(id)).collect(Collectors.toList()));
+        });
         return Result.success();
     }
 
-    @ApiOperation(value = "修改商品分类(部分更新)", httpMethod = "PATCH")
+    @ApiOperation(value = "修改商品分类", httpMethod = "PATCH")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", value = "用户id", required = true, paramType = "path", dataType = "Long"),
             @ApiImplicitParam(name = "category", value = "实体JSON对象", required = true, paramType = "body", dataType = "PmsCategory")
     })
     @PatchMapping(value = "/{id}")
     public Result patch(@PathVariable Integer id, @RequestBody PmsCategory category) {
-        LambdaUpdateWrapper<PmsCategory> luw = new LambdaUpdateWrapper<PmsCategory>().eq(PmsCategory::getId, id);
-        if (category.getStatus() != null) { // 状态更新
-            luw.set(PmsCategory::getStatus, category.getStatus());
-        }
-        boolean update = iPmsCategoryService.update(luw);
+        LambdaUpdateWrapper<PmsCategory> updateWrapper = new LambdaUpdateWrapper<PmsCategory>().eq(PmsCategory::getId, id);
+        updateWrapper.set(category.getStatus() != null, PmsCategory::getStatus, category.getStatus());
+        boolean update = iPmsCategoryService.update(updateWrapper);
         return Result.success(update);
     }
 }
