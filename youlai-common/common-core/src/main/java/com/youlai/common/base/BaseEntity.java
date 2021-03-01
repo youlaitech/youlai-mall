774fc73c1f0b4a4bedcade5b95378ca3d54204fc
hxrui <1490493387@qq.com>
2021-03-01 13:52:17 +0800

    feat:Oauth2整合knife4j

--- youlai-common/common-core/src/main/java/com/youlai/common/base/BaseEntity.java
+++ youlai-common/common-core/src/main/java/com/youlai/common/base/BaseEntity.java
@@ -4,8 +4,6 @@ package com.youlai.common.base;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -13,17 +11,14 @@ import java.util.Date;
 
 
 @Data
-@ApiModel
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty(value = "创建时间", example = "2020-01-01 00:00:00")
     @JsonInclude(value = JsonInclude.Include.NON_NULL)
     private Date gmtCreate;
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    @ApiModelProperty(value = "创建时间", example = "2020-01-01 00:00:00")
     @JsonInclude(value = JsonInclude.Include.NON_NULL)
     private Date gmtModified;
 }
