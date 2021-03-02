8f9f9d1fa6d01df91921d3020559a469d62120ba
haoxr <1490493387@qq.com>
2021-03-02 16:17:00 +0800

    doc(youlai.sql):更新sql脚本

--- youlai-common/common-web/src/main/java/com/youlai/common/web/aspect/LoginLogAspect.java
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/aspect/LoginLogAspect.java
@@ -1,15 +1,14 @@
-package com.youlai.common.web.log;
+package com.youlai.common.web.aspect;
 
 import cn.hutool.json.JSONUtil;
+import com.youlai.common.web.pojo.domain.LoginLog;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,15 +20,14 @@ import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 
 /**
- * @Author haoxr
- * @Date 2021-03-01 16:47
- * @Version 1.0.0
+ * @Author HXR
+ * @CreateTime 2021-03-01 16:47
  */
 @Aspect
 @Component
 @AllArgsConstructor
 @Slf4j
-public class LogAspect {
+public class LoginLogAspect {
 
     private RedisTemplate redisTemplate;
 
@@ -39,24 +37,24 @@ public class LogAspect {
 
     @Around("Log()")
     public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("日志记录");
+        log.info("登录日志记录");
         long startTime = System.nanoTime();
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        Log log = new Log();
+        LoginLog loginLog = new LoginLog();
         Signature signature = joinPoint.getSignature();
         MethodSignature methodSignature = (MethodSignature) signature;
         Method method = methodSignature.getMethod();
         if (method.isAnnotationPresent(ApiOperation.class)) {
             ApiOperation apiOperation = method.getAnnotation(ApiOperation.class);
-            log.setDescription(apiOperation.value());
+            loginLog.setDescription(apiOperation.value());
         }
         long endTime = System.nanoTime();
-        log.setIntervalTime((int) (endTime - startTime));
-        log.setIp(request.getRemoteUser());
-        log.setUrl(request.getRequestURL().toString());
-        log.setMethod(request.getMethod());
-        redisTemplate.opsForSet().add("sys:log", JSONUtil.toJsonStr(log));
+        loginLog.setElapsedTime((int) (endTime - startTime));
+        loginLog.setIp(request.getRemoteUser());
+        loginLog.setUrl(request.getRequestURL().toString());
+        loginLog.setMethod(request.getMethod());
+        redisTemplate.opsForSet().add("log:login", JSONUtil.toJsonStr(loginLog));
         Object result = joinPoint.proceed();
         return result;
     }
