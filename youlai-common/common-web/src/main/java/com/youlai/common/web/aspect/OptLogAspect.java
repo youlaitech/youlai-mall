a119c0af75201b02abc9e486ec8b6dcb93e151f3
haoxr <1490493387@qq.com>
2021-03-02 20:40:38 +0800

    feat:整合elk

--- youlai-common/common-web/src/main/java/com/youlai/common/web/aspect/OptLogAspect.java
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/aspect/OptLogAspect.java
@@ -2,22 +2,27 @@ package com.youlai.common.web.aspect;
 
 import cn.hutool.json.JSONUtil;
 import com.youlai.common.web.pojo.domain.LoginLog;
+import com.youlai.common.web.pojo.domain.OptLog;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.marker.Markers;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author HXR
@@ -26,36 +31,43 @@ import java.lang.reflect.Method;
 @Aspect
 @Component
 @AllArgsConstructor
-@Slf4j
 public class OptLogAspect {
 
-    private RedisTemplate redisTemplate;
-
-    @Pointcut("execution(public * com.youlai..*.controller.*.*(..))")
+    private static final Logger LOGGER = LoggerFactory.getLogger(OptLogAspect.class);
+    @Pointcut("execution(public * com.youlai..*.controller.*.*(..)) || execution(public * com.youlai.*.controller.*(..))")
     public void Log() {
     }
 
     @Around("Log()")
     public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("操作日志记录");
         long startTime = System.nanoTime();
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        LoginLog loginLog = new LoginLog();
+        OptLog optLog = new OptLog();
         Signature signature = joinPoint.getSignature();
         MethodSignature methodSignature = (MethodSignature) signature;
         Method method = methodSignature.getMethod();
         if (method.isAnnotationPresent(ApiOperation.class)) {
             ApiOperation apiOperation = method.getAnnotation(ApiOperation.class);
-            loginLog.setDescription(apiOperation.value());
+            optLog.setDescription(apiOperation.value());
         }
         long endTime = System.nanoTime();
-        loginLog.setElapsedTime((int) (endTime - startTime));
-        loginLog.setIp(request.getRemoteUser());
-        loginLog.setUrl(request.getRequestURL().toString());
-        loginLog.setMethod(request.getMethod());
-        redisTemplate.opsForSet().add("log:opt", JSONUtil.toJsonStr(loginLog));
+        optLog.setElapsedTime((int) (endTime - startTime));
+        optLog.setIp(request.getRemoteUser());
+        optLog.setUrl(request.getRequestURL().toString());
+        optLog.setMethod(request.getMethod());
         Object result = joinPoint.proceed();
+        optLog.setResult(result);
+
+        Map<String,Object> logMap = new HashMap<>();
+        logMap.put("url",optLog.getUrl());
+        logMap.put("method",optLog.getMethod());
+        logMap.put("elapsedTime",optLog.getElapsedTime());
+        logMap.put("description",optLog.getDescription());
+        logMap.put("result",result);
+
+        LOGGER.info("123");
+        LOGGER.info(Markers.appendEntries(logMap),JSONUtil.parse(optLog).toString());
         return result;
     }
 }
