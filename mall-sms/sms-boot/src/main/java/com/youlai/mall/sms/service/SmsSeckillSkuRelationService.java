a905d5b3f81858af83700a5ef41689a80a01e820
dongtiandexue <huawei_code@163.com>
2021-03-05 00:11:11 +0800

    feat:秒杀活动管理员端结构搭建

---
+++ mall-sms/sms-boot/src/main/java/com/youlai/mall/sms/service/SmsSeckillSkuRelationService.java
@@ -0,0 +1,13 @@
+package com.youlai.mall.sms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.youlai.mall.sms.pojo.domain.SmsSeckillSkuRelation;
+
+/**
+ * @author huawei
+ * @desc 秒杀活动场次商品关联业务接口
+ * @email huawei_code@163.com
+ * @date 2021/3/5
+ */
+public interface SmsSeckillSkuRelationService extends IService<SmsSeckillSkuRelation> {
+}
---
+++ mall-sms/sms-boot/src/main/java/com/youlai/mall/sms/service/impl/SmsSeckillSessionServiceImpl.java
@@ -0,0 +1,19 @@
+package com.youlai.mall.sms.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.youlai.mall.sms.mapper.SmsSeckillSessionMapper;
+import com.youlai.mall.sms.pojo.domain.SmsSeckillSession;
+import com.youlai.mall.sms.service.SmsSeckillSessionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author huawei
+ * @desc 秒杀活动场次管理业务实现类
+ * @email huawei_code@163.com
+ * @date 2021/3/5
+ */
+@Service
+@Slf4j
+public class SmsSeckillSessionServiceImpl extends ServiceImpl<SmsSeckillSessionMapper, SmsSeckillSession> implements SmsSeckillSessionService {
+}
