512b92cd7d60b502389d25b0cbc3f46558d560b1
haoxr <1490493387@qq.com>
2021-03-09 16:38:55 +0800

    feat:会话强制下线

--- youlai-common/common-web/src/main/java/com/youlai/common/web/util/IPUtils.java
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/util/IPUtils.java
@@ -2,20 +2,29 @@ package com.youlai.common.web.util;
 
 import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.lionsoul.ip2region.DataBlock;
+import org.lionsoul.ip2region.DbConfig;
+import org.lionsoul.ip2region.DbSearcher;
+import org.lionsoul.ip2region.Util;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @Slf4j
-public class IpUtils {
+public class IPUtils {
+
+    private static String LOCAL_IP="127.0.0.1";
 
     /**
      * 获取IP地址
      * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
      * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
      */
-    public static String getIpAddr(HttpServletRequest request) {
+    public static String getClientIP(HttpServletRequest request) {
         String ip = null;
         try {
             if (request == null) {
@@ -46,10 +55,8 @@ public class IpUtils {
         }
 
         //使用代理，则获取第一个IP地址
-        if (StrUtil.isEmpty(ip) && ip.length() > 15) {
-            if (ip.indexOf(",") > 0) {
-                ip = ip.substring(0, ip.indexOf(","));
-            }
+        if (StrUtil.isNotBlank(ip) && ip.indexOf(",") > 0) {
+            ip = ip.substring(0, ip.indexOf(","));
         }
 
         return ip;
@@ -71,4 +78,43 @@ public class IpUtils {
         }
         return null;
     }
+
+
+    /**
+     * 解析IP获取城市区域信息
+     *
+     * @param ip
+     * @return
+     */
+    public static String ip2region(String ip) {
+        if (Util.isIpAddress(ip) == false) {
+            return Strings.EMPTY;
+        }
+
+        if(LOCAL_IP.equals(ip)){
+            return "本地访问";
+        }
+
+        String filePath = IPUtils.class.getResource("/data/ip2region.db").getPath();
+        File file = new File(filePath);
+        if (file.exists() == false) {
+            return Strings.EMPTY;
+        }
+
+        try {
+            DbConfig config = new DbConfig();
+            DbSearcher searcher = new DbSearcher(config, filePath);
+
+            Method method = searcher.getClass().getMethod("btreeSearch", String.class);
+            DataBlock dataBlock = (DataBlock) method.invoke(searcher, ip);
+
+            return dataBlock.getRegion();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Strings.EMPTY;
+    }
+
+
 }
