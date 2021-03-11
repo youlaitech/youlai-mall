10be6239eb2bc085407664cdd8d9734525f022c0
haoxr <1490493387@qq.com>
2021-03-12 01:18:30 +0800

    feat:loadbalancer替换ribbon

--- youlai-admin/admin-api/src/main/java/com/youlai/admin/api/UserFeignService.java
+++ youlai-admin/admin-api/src/main/java/com/youlai/admin/api/UserFeignService.java
@@ -2,7 +2,6 @@ package com.youlai.admin.api;
 
 import com.youlai.admin.pojo.dto.UserDTO;
 import com.youlai.common.result.Result;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
