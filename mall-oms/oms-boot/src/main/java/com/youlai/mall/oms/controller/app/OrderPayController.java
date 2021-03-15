98ade8d3b05cea38f0b8142662987f2a74d2ab39
haoxr <1490493387@qq.com>
2021-03-15 20:10:26 +0800

    feat:整理订单模块代码

--- mall-oms/oms-boot/src/main/java/com/youlai/mall/oms/controller/app/OrderPayController.java
+++ mall-oms/oms-boot/src/main/java/com/youlai/mall/oms/controller/app/OrderPayController.java
@@ -6,9 +6,8 @@ import com.youlai.mall.oms.enums.PayTypeEnum;
 import com.youlai.mall.oms.pojo.form.OrderPayForm;
 import com.youlai.mall.oms.pojo.vo.PayInfoVO;
 import com.youlai.mall.oms.service.IOrderPayService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -24,10 +23,11 @@ import org.springframework.web.bind.annotation.*;
  */
 @Api(tags = "【移动端】订单支付")
 @RestController
-@RequestMapping("/api.app/v1/order/pay")
+@RequestMapping("/api.app/v1/order_pays")
 @Slf4j
+@AllArgsConstructor
 public class OrderPayController {
-    @Autowired
+
     private IOrderPayService orderPayService;
 
     /**
@@ -36,29 +36,39 @@ public class OrderPayController {
      * 2、根据订单ID查询订单价格，进行支付（在整个支付的过程中进行事务控制，保证整个操作的原子性）
      * 3、将支付结果记录日志并返回给前端
      *
-     * @param orderPayForm 订单支付表单
      * @return
      */
     @ApiOperation("订单支付")
     @PostMapping
-    public Result doPay(@Validated @RequestBody OrderPayForm orderPayForm) {
-        PayTypeEnum payTypeEnum = PayTypeEnum.getValue(orderPayForm.getPayType());
-        if (payTypeEnum == null) {
-            return Result.failed("请选择正确的支付方式");
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "payType", value = "支付方式", paramType = "query", dataType = "Integer"),
+            @ApiImplicitParam(name = "orderId", value = "订单ID", paramType = "query", dataType = "Long")
+    })
+    public Result pay(
+            Integer payType,
+            Long orderId
+    ) {
+        PayTypeEnum payTypeEnum = PayTypeEnum.getValue(payType);
+
+        boolean result = false;
+        switch (payTypeEnum) {
+            case ALIPAY:
+            case WEIXIN:
+                // TODO
+                break;
+            case BALANCE:
+                result = orderPayService.payWithBalance(orderId);
+                break;
+            default:
+                return Result.failed("系统暂不支持该支付方式~");
         }
-        log.info("订单支付，orderId={}，支付方式={}", orderPayForm.getOrderId(), payTypeEnum.getText());
-        if (payTypeEnum == PayTypeEnum.BALANCE) {
-            orderPayService.balancePay(orderPayForm.getOrderId());
-        }
-        return Result.success();
+        return Result.judge(result);
     }
 
     @ApiOperation(value = "获取订单支付详情")
-    @GetMapping("/info")
-    public Result<PayInfoVO> info(
-            @ApiParam(name = "orderId", value = "订单ID", required = true, defaultValue = "1")
-            @RequestParam("orderId") Long orderId) {
-        return Result.success(orderPayService.info(orderId));
+    @GetMapping("/orderId/{orderId}")
+    public Result<PayInfoVO> detail(@PathVariable Long orderId) {
+        orderPayService.getByOrderId(orderId);
+        return Result.success();
     }
-
 }
