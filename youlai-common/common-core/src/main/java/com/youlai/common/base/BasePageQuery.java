e358f7460e4d642afd7787e8aa9e9d662febea8e
dongtiandexue <huawei_code@163.com>
2021-03-15 23:47:48 +0800

    feat:优惠券服务管理员端搭建

---
+++ youlai-common/common-core/src/main/java/com/youlai/common/base/BasePageQuery.java
@@ -0,0 +1,20 @@
+package com.youlai.common.base;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author huawei
+ * @desc 基础分页请求对象
+ * @email huawei_code@163.com
+ * @date 2021/2/28
+ */
+@Data
+public class BasePageQuery {
+
+    @ApiModelProperty(value = "当前页", example = "1")
+    private int pageNum = 1;
+
+    @ApiModelProperty(value = "每页记录数", example = "10")
+    private int pageSize = 10;
+}
