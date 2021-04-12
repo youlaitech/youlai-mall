acad847789fb0ba3bce7b1c447281a6d419c008e
haoxr <1490493387@qq.com>
2021-04-13 00:26:47 +0800

    feat:Sentinel普通流控整合

---
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/component/CustomBlockExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.youlai.admin.component;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.youlai.common.result.Result;
+import com.youlai.common.result.ResultCode;
+import jodd.net.HttpStatus;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author haoxr
+ * @description TODO
+ * @createTime 2021/4/12 22:57
+ */
+@Component
+public class CustomBlockExceptionHandler implements BlockExceptionHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, BlockException e) throws Exception {
+        response.setStatus(HttpStatus.ok().status());
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("Content-Type", "application/json;charset=utf-8");
+        response.setContentType("application/json;charset=utf-8");
+
+        if(e instanceof FlowException){
+            new ObjectMapper().writeValue(response.getWriter(), Result.failed(ResultCode.FLOW_LIMITING));
+        }
+    }
+}
