4652047dc2f7cd2ea6833d044b48207c6342c1e4
haoxr <1490493387@qq.com>
2021-06-08 01:27:17 +0800

    feat:微信小程序用户登录逻辑

--- mall-oms/oms-api/src/main/java/com/youlai/mall/oms/api/OrderFeignClient.java
+++ mall-oms/oms-api/src/main/java/com/youlai/mall/oms/api/OrderFeignClient.java
@@ -1,10 +1,6 @@
 package com.youlai.mall.oms.api;
 
-import com.youlai.common.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author haoxr
