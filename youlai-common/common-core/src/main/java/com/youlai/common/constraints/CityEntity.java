4b1158a0653e7a081775934284329a073a4cbdd4
Gadfly <gadfly@gadfly.vip>
2021-08-06 18:21:27 +0800

    feat: 增加Validation配置，重构部分地址Controller，增加针对地址的自定义校验器

---
+++ youlai-common/common-core/src/main/java/com/youlai/common/constraints/CityEntity.java
@@ -0,0 +1,14 @@
+package com.youlai.common.constraints;
+
+import lombok.Data;
+
+/**
+ * @author Gadfly
+ * @since 2021-08-06 16:46
+ */
+@Data
+public class CityEntity {
+    private String value;
+    private String name;
+    private String parent;
+}
