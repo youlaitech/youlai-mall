880511810b9b3d92b85da30ac606e818ba8521e5
Gadfly <gadfly@gadfly.vip>
2021-08-11 18:07:13 +0800

    feat: 用户商品浏览历史
    用redis zset实现的最近十条记录（高并发时可能会有误差），满足我目前的需求了。想要京东淘宝那样丰富的可能需要借助es。

--- mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/PmsApplication.java
+++ mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/PmsApplication.java
@@ -1,13 +1,16 @@
 package com.youlai.mall.pms;
 
+import com.youlai.mall.ums.api.MemberFeignClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
+@EnableFeignClients(basePackageClasses = {MemberFeignClient.class})
 @EnableTransactionManagement
 public class PmsApplication {
     public static void main(String[] args) {
