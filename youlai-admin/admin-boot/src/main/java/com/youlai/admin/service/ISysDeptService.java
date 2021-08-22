b2eda9c7eb2c3506f4a1273cb7c13db4e203d018
有来技术 <1490493387@qq.com>
2021-08-22 21:54:08 +0800

    refactor(DeptController.java): 部门接口重构优化

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/service/ISysDeptService.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/service/ISysDeptService.java
@@ -1,17 +1,47 @@
 package com.youlai.admin.service;
 
-
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.youlai.admin.pojo.entity.SysDept;
 import com.youlai.admin.pojo.vo.DeptVO;
 import com.youlai.admin.pojo.vo.TreeVO;
-
 import java.util.List;
 
+/**
+ * 菜单控制器
+ *
+ * @author <a href="mailto:xianrui0365@163.com">xianrui</a>
+ * @date 2021-08-22
+ */
 public interface ISysDeptService extends IService<SysDept> {
+    /**
+     * 部门表格（Table）层级列表
+     *
+     * @param status 部门状态： 1-开启 0-禁用
+     * @param name
+     * @return
+     */
+    List<DeptVO> listTable(Integer status, String name);
 
-    List<DeptVO> listDeptVO(LambdaQueryWrapper<SysDept> baseQuery);
+    /**
+     * 部门下拉（Select）层级列表
+     *
+     * @return
+     */
+    List<TreeVO> listSelect();
 
-    List<TreeVO> listTreeVO(LambdaQueryWrapper<SysDept> baseQuery);
+    /**
+     * 新增/修改部门
+     *
+     * @param dept
+     * @return
+     */
+    Long saveDept(SysDept dept);
+
+    /**
+     * 删除部门
+     *
+     * @param ids 部门ID，多个以英文逗号,拼接字符串
+     * @return
+     */
+    boolean deleteByIds(String ids);
 }
