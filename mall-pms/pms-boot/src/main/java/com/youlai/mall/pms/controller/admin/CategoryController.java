b4ef8054d5a32b17d05a6c8e08676f4b38d19fdf
有来技术 <1490493387@qq.com>
2021-08-22 21:56:10 +0800

    feat(CategoryController.java): 商品分类整合Spring Cache Redis缓存

--- mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/controller/admin/CategoryController.java
+++ mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/controller/admin/CategoryController.java
@@ -10,9 +10,9 @@ import com.youlai.mall.pms.service.IPmsAttributeService;
 import com.youlai.mall.pms.service.IPmsCategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -57,27 +57,24 @@ public class CategoryController {
     }
 
     @ApiOperation(value = "新增分类")
-    @ApiImplicitParam(name = "category", value = "实体JSON对象", required = true, paramType = "body", dataType = "PmsCategory")
     @PostMapping
     public Result add(@RequestBody PmsCategory category) {
-        boolean result = iPmsCategoryService.save(category);
-        return Result.judge(result);
+        Long id = iPmsCategoryService.saveCategory(category);
+        return Result.success(id);
     }
 
     @ApiOperation(value = "修改分类")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "商品分类id", required = true, paramType = "path", dataType = "Long"),
-            @ApiImplicitParam(name = "category", value = "实体JSON对象", required = true, paramType = "body", dataType = "PmsCategory")
-    })
     @PutMapping(value = "/{id}")
-    public Result update(@RequestBody PmsCategory category) {
-        boolean result = iPmsCategoryService.updateById(category);
-        return Result.judge(result);
+    public Result update(@PathVariable Long id, @RequestBody PmsCategory category) {
+        category.setId(id);
+        id = iPmsCategoryService.saveCategory(category);
+        return Result.success(id);
     }
 
     @ApiOperation(value = "删除分类")
     @ApiImplicitParam(name = "ids", value = "id集合,以英文逗号','分隔", required = true, paramType = "query", dataType = "String")
     @DeleteMapping("/{ids}")
+    @CacheEvict(value = "pms",key = "'categoryList'")
     public Result delete(@PathVariable String ids) {
         List<String> categoryIds = Arrays.asList(ids.split(","));
         Optional.ofNullable(categoryIds).ifPresent(categoryIdList -> {
@@ -88,11 +85,8 @@ public class CategoryController {
     }
 
     @ApiOperation(value = "选择性修改分类")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "分类ID", required = true, paramType = "path", dataType = "Long"),
-            @ApiImplicitParam(name = "category", value = "实体JSON对象", required = true, paramType = "body", dataType = "PmsCategory")
-    })
     @PatchMapping(value = "/{id}")
+    @CacheEvict(value = "pms",key = "'categoryList'")
     public Result patch(@PathVariable Long id, @RequestBody PmsCategory category) {
         LambdaUpdateWrapper<PmsCategory> updateWrapper = new LambdaUpdateWrapper<PmsCategory>().eq(PmsCategory::getId, id);
         updateWrapper.set(category.getVisible() != null, PmsCategory::getVisible, category.getVisible());
