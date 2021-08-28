9bcdc9bfaec4b6d9272f88e860d19d17f45d6f7b
有来技术 <1490493387@qq.com>
2021-08-29 00:59:23 +0800

    refactor(AdminApplication): 移除log4j2的bootstrap.yml的变量监听

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/AdminApplication.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/AdminApplication.java
@@ -1,21 +1,13 @@
 package com.youlai.admin;
 
-import com.youlai.common.web.listener.LoggingListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationListener;
-
-import java.util.Set;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AdminApplication {
     public static void main(String[] args) {
-
-        SpringApplication app =new SpringApplication(AdminApplication.class);
-        LoggingListener loggingListener = new LoggingListener();
-        app.addListeners(loggingListener);
-        app.run(args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 }
