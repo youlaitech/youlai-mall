448a14a0c2224c07b8e5944c2a15bd436367c6f0
有来技术 <1490493387@qq.com>
2021-08-29 01:08:19 +0800

    style(ISysUserService.java): 补充用户操作注释

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/service/ISysUserService.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/service/ISysUserService.java
@@ -8,11 +8,36 @@ import com.youlai.admin.pojo.entity.SysUser;
 
 public interface ISysUserService extends IService<SysUser> {
 
-    IPage<SysUser> list(Page<SysUser> page, SysUser sysUser);
+    /**
+     * 用户分页列表
+     * @param page
+     * @param user
+     * @return
+     */
+    IPage<SysUser> list(Page<SysUser> page, SysUser user);
 
+    /**
+     * 新增用户
+     *
+     * @param user
+     * @return
+     */
     boolean saveUser(SysUser user);
 
+    /**
+     * 修改用户
+     *
+     * @param user
+     * @return
+     */
     boolean updateUser(SysUser user);
 
-    SysUser getByUsername(String username);
+    /**
+     * 根据用户名获取认证用户信息，携带角色和密码
+     *
+     * @param username
+     * @return
+     */
+     SysUser getByUsername(String username);
+
 }
