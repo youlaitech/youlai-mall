3fd6bb1236144bf36839c6d0bb701935a042d2d8
有来技术 <1490493387@qq.com>
2021-08-31 23:12:00 +0800

    refactor(SecurityGlobalFilter.java): 线上环境放行注销接口和禁止新增路由操作

--- youlai-gateway/src/main/java/com/youlai/gateway/swagger/SwaggerHandler.java
+++ youlai-gateway/src/main/java/com/youlai/gateway/swagger/SwaggerHandler.java
@@ -14,7 +14,7 @@ import java.util.Optional;
  * @author  xianrui
  * @Date 2021-02-25 16:34
  * @Version 1.0.0
- * @ https://gitee.com/xiaoym/swagger-bootstrap-ui-demo/blob/master/knife4j-spring-cloud-gateway/service-doc/src/main/java/com/xiaominfo/swagger/service/doc/handler/SwaggerHandler.java
+ * @ https://gitee.com/xiaoym/swagger-bootstrap-ui-demo/blob/masterknife4j-spring-cloud-gateway/service-doc/src/main/java/com/xiaominfo/swagger/service/doc/handler/SwaggerHandler.java
  */
 @RestController
 public class SwaggerHandler {
