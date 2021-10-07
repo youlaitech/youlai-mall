62181943cc43887db2c8023a403de0ef372c02c5
xchb <704566072@qq.com>
2021-10-07 12:23:35 +0800

    解决gateway跨域问题

---
+++ youlai-gateway/src/main/java/com/youlai/gateway/config/CorsAutoConfiguration.java
@@ -0,0 +1,31 @@
+package com.youlai.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+/**
+ * @author XDD
+ * @project youlai-mall
+ * @date 2021/10/7
+ * @description Good Good Study,Day Day Up.
+ */
+@Configuration
+public class CorsAutoConfiguration {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
