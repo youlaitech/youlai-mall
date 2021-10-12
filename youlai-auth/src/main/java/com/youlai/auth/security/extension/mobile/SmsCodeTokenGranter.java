9c1758e9dc67baae54eb5b5776f5e503a6419ed0
有来技术 <1490493387@qq.com>
2021-10-12 23:56:09 +0800

    style(SmsCodeTokenGranter):注释添加

--- youlai-auth/src/main/java/com/youlai/auth/security/extension/mobile/SmsCodeTokenGranter.java
+++ youlai-auth/src/main/java/com/youlai/auth/security/extension/mobile/SmsCodeTokenGranter.java
@@ -14,13 +14,21 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * 手机验证码
+ * 手机验证码授权者
  *
  * @author <a href="mailto:xianrui0365@163.com">xianrui</a>
  * @date 2021/9/25
  */
 public class SmsCodeTokenGranter extends AbstractTokenGranter {
 
+    /**
+     * 声明授权者 CaptchaTokenGranter 支持授权模式 sms_code
+     * 根据接口传值 grant_type = sms_code 的值匹配到此授权者
+     * 匹配逻辑详见下面的两个方法
+     *
+     * @see org.springframework.security.oauth2.provider.CompositeTokenGranter#grant(String, TokenRequest)
+     * @see org.springframework.security.oauth2.provider.token.AbstractTokenGranter#grant(String, TokenRequest)
+     */
     private static final String GRANT_TYPE = "sms_code";
     private final AuthenticationManager authenticationManager;
 
