65ebc62f604d172b450e32ea4787555c76482f02
chuan <2256222053@qq.com>
2021-12-10 18:52:18 +0800

    feat: 添加数据权限拦截器
    添加数据权限拦截器

---
+++ youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/handler/InterceptorIgnore.java
@@ -0,0 +1,23 @@
+package com.youlai.common.mybatis.handler;
+
+import java.lang.annotation.*;
+
+/**
+ * 是否需要数据权限
+ *
+ * @author <a href="mailto:2256222053@qq.com">zc</a>
+ * @Date 2021-12-10 15:48
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface InterceptorIgnore {
+
+    /**
+     * 数据权限 {@link com.baomidou.mybatisplus.extension.plugins.inner.DataPermissionInterceptor}
+     * <p>
+     * 默认打开，需要注解关闭
+     */
+    String dataPermission() default "0";
+}
+
