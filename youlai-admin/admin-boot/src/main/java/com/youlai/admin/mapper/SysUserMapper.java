84cfb4558a0b2eefb3a000b79557f89ac2ff8b0b
chuan <2256222053@qq.com>
2021-12-14 20:41:24 +0800

    feat: 部门权限添加别名属性，用户接口添加查询关键字
    部门权限添加别名属性，用户接口添加(名称/昵称/手机号码)查询关键字

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/mapper/SysUserMapper.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/mapper/SysUserMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.youlai.admin.dto.UserAuthDTO;
 import com.youlai.admin.pojo.entity.SysUser;
+import com.youlai.common.mybatis.handler.InterceptorIgnore;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
 
+    @InterceptorIgnore(storeAlias = "u")
     List<SysUser> list(Page<SysUser> page, SysUser user);
 
     UserAuthDTO getByUsername(String username);
