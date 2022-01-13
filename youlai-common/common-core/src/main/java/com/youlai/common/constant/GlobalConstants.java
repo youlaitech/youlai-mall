e845ed5b561c34eaf237b672ed67c6ce2e7ebfe5
郝先瑞 <1490493387@qq.com>
2022-01-13 22:52:53 +0800

    style(GlobalConstants): 全局常量添加注释说明

--- youlai-common/common-core/src/main/java/com/youlai/common/constant/GlobalConstants.java
+++ youlai-common/common-core/src/main/java/com/youlai/common/constant/GlobalConstants.java
@@ -1,19 +1,36 @@
 package com.youlai.common.constant;
 
 /**
- *  全局常量
- *  @author <a href="mailto:xianrui0365@163.com">haoxr</a>
- *  @date 2021/10/30 9:32
+ * 全局常量
+ *
+ * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2021/10/30 9:32
  */
 public interface GlobalConstants {
 
+    /**
+     * 全局状态-是
+     */
     Integer STATUS_YES = 1;
 
+    /**
+     * 系统默认密码
+     */
     String DEFAULT_USER_PASSWORD = "123456";
 
+    /**
+     * 超级管理员角色编码
+     */
     String ROOT_ROLE_CODE = "ROOT";
 
-    String URL_PERM_ROLES_KEY = "system:perm_roles_rule:url:";
-    String BTN_PERM_ROLES_KEY = "system:perm_roles_rule:btn:";
+    /**
+     * [ {接口路径:[角色编码]},...]
+     */
+    String URL_PERM_ROLES_KEY = "system:perm_roles_rule:url";
+
+    /**
+     * [{按钮权限:[角色编码]},...]
+     */
+    String BTN_PERM_ROLES_KEY = "system:perm_roles_rule:btn";
 
 }
