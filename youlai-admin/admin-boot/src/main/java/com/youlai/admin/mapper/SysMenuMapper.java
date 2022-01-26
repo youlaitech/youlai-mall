d9e6960b959038a60a658c2afb545d8ae7041a9e
郝先瑞 <1490493387@qq.com>
2022-01-26 21:14:39 +0800

    refactor: listRoute方法名调整为listRoutes

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/mapper/SysMenuMapper.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/mapper/SysMenuMapper.java
@@ -1,22 +1,24 @@
 package com.youlai.admin.mapper;
 
+/**
+ * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2022/1/24 21:52
+ */
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.youlai.admin.pojo.entity.SysMenu;
-import org.apache.ibatis.annotations.Many;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 /**
+ *
  * @author haoxr
  * @date 2020-11-06
  */
 @Mapper
 public interface SysMenuMapper extends BaseMapper<SysMenu> {
 
-    List<SysMenu> listRoute();
+    List<SysMenu> listRoutes();
 
 }
