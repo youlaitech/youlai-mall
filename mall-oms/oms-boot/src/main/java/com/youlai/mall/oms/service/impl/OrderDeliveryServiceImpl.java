9f5c5665fd8517c31571b1dc068e1b71d45396ce
郝先瑞 <1490493387@qq.com>
2022-01-28 19:37:57 +0800

    fix(OrderDeliveryServiceImpl.java): 代码引用多了分号
    Closes #I4SGQP

--- mall-oms/oms-boot/src/main/java/com/youlai/mall/oms/service/impl/OrderDeliveryServiceImpl.java
+++ mall-oms/oms-boot/src/main/java/com/youlai/mall/oms/service/impl/OrderDeliveryServiceImpl.java
@@ -1,6 +1,6 @@
 package com.youlai.mall.oms.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.youlai.mall.oms.mapper.OrderDeliveryMapper;
 import com.youlai.mall.oms.pojo.entity.OmsOrderDelivery;
 import com.youlai.mall.oms.service.IOrderDeliveryService;
