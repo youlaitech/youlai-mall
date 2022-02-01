25da36eeb9c1dec3af7315114a93c7f53b0b847f
郝先瑞 <1490493387@qq.com>
2022-02-02 00:00:44 +0800

    refactor: 订单分页列表移至mapper.xml

---
+++ mall-oms/oms-boot/src/main/java/com/youlai/mall/oms/pojo/query/OrderPageQuery.java
@@ -0,0 +1,26 @@
+package com.youlai.mall.oms.pojo.query;
+
+import com.youlai.common.base.BasePageQuery;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+
+/**
+ * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2022/2/1 19:14
+ */
+@Data
+@ApiModel("订单分页查询对象")
+public class OrderPageQuery extends BasePageQuery {
+
+    @ApiModelProperty("订单状态")
+    private Integer status;
+
+    @ApiModelProperty(value = "开始时间(格式：yyyy-MM-dd)")
+    private String beginDate;
+
+    @ApiModelProperty(value = "截止时间(格式：yyyy-MM-dd)")
+    private String endDate;
+
+}
---
+++ mall-oms/oms-boot/src/main/java/com/youlai/mall/oms/pojo/vo/OrderPageVO.java
@@ -0,0 +1,62 @@
+package com.youlai.mall.oms.pojo.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 订单分页对象
+ *
+ * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2022/2/1 20:58
+ */
+@Data
+public class OrderPageVO {
+
+    private Long id;
+
+    private String orderSn;
+
+    private Long totalAmount;
+
+    private Long payAmount;
+
+    private Integer payType;
+
+    private Integer status;
+
+    private Integer totalQuantity;
+
+    private Date gmtCreate;
+
+    private Long memberId;
+
+    private Integer sourceType;
+
+    private List<OrderItem> orderItems;
+
+    @Data
+    public static class OrderItem {
+
+        private Long id;
+
+        private Long orderId;
+
+        private Long skuId;
+
+        private String skuName;
+
+        private String skuPic;
+
+        private Long skuPrice;
+
+        private Integer skuQuantity;
+
+        private Long skuTotalPrice;
+
+        private String spuName;
+
+    }
+
+}
