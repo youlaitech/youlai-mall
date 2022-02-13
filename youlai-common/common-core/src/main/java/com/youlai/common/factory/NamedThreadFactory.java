4bfd3bbfaeb532cfdc292df5f7244989e069ed28
郝先瑞 <1490493387@qq.com>
2022-02-13 12:20:09 +0800

    feat(NamedThreadFactory.java): 自定义 ThreadFactory 实现线程重命名

---
+++ youlai-common/common-core/src/main/java/com/youlai/common/factory/NamedThreadFactory.java
@@ -0,0 +1,45 @@
+package com.youlai.common.factory;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 自定义 ThreadFactory 实现线程重命名
+ *
+ * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2022/2/13 0:14
+ */
+public class NamedThreadFactory implements ThreadFactory {
+
+    private final AtomicInteger poolNumber = new AtomicInteger(1);
+
+    private final ThreadGroup threadGroup;
+
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+    public final String namePrefix;
+
+    public NamedThreadFactory(String name) {
+        SecurityManager s = System.getSecurityManager();
+        threadGroup = (s != null) ? s.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+        if (null == name || "".equals(name.trim())) {
+            name = "pool";
+        }
+        namePrefix = name + "-" +
+                poolNumber.getAndIncrement() +
+                "-thread-";
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(threadGroup, r,
+                namePrefix + threadNumber.getAndIncrement(),
+                0);
+        if (t.isDaemon())
+            t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
