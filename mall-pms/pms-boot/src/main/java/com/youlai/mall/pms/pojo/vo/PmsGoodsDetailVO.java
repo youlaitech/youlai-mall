3cf12a88f6cdfdb0d33b1cc25daef79698e26725
郝先瑞 <1490493387@qq.com>
2022-03-13 22:10:41 +0800

    docs(PmsGoodsDetailVO.java): 商品详情视图对象注释添加

--- mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/pojo/vo/PmsGoodsDetailVO.java
+++ mall-pms/pms-boot/src/main/java/com/youlai/mall/pms/pojo/vo/PmsGoodsDetailVO.java
@@ -1,9 +1,10 @@
-package com.youlai.mall.pms.pojo.vo.admin;
+package com.youlai.mall.pms.pojo.vo;
 
 import com.youlai.mall.pms.pojo.entity.PmsSku;
 import com.youlai.mall.pms.pojo.entity.PmsSpuAttributeValue;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.ToString;
 
 import java.util.List;
 
@@ -11,10 +12,11 @@ import java.util.List;
  * 商品详情视图对象
  *
  * @author <a href="mailto:xianrui0365@163.com">haoxr</a>
+ * @date 2022/3/13
  */
 @Data
-@ToString
-public class GoodsDetailVO {
+@ApiModel("商品详情视图对象")
+public class PmsGoodsDetailVO {
 
     private Long id;
 
@@ -28,8 +30,10 @@ public class GoodsDetailVO {
 
     private Long price;
 
+    @ApiModelProperty("商品主图")
     private String picUrl;
 
+    @ApiModelProperty("商品副图")
     private String[] subPicUrls;
 
     private String description;
