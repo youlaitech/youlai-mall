3815fa794633ee7ae2cf81422f72d303777f611f
郝先瑞 <1490493387@qq.com>
2022-03-13 15:24:17 +0800

    refactor(PageResult.java):  分页的总条数调整至data的total属性，数据列表移动至data的list属性。

--- youlai-common/common-core/src/main/java/com/youlai/common/result/PageResult.java
+++ youlai-common/common-core/src/main/java/com/youlai/common/result/PageResult.java
@@ -17,18 +17,30 @@ public class PageResult<T> implements Serializable {
 
     private String code;
 
-    private List<T> data;
-
-    private long total;
+    private Data data;
 
     private String msg;
 
     public static <T> PageResult<T> success(IPage<T> page) {
         PageResult<T> result = new PageResult<>();
         result.setCode(ResultCode.SUCCESS.getCode());
-        result.setData(page.getRecords());
-        result.setTotal(page.getTotal());
+
+        Data data = new Data<T>();
+        data.setList(page.getRecords());
+        data.setTotal(page.getTotal());
+
+        result.setData(data);
         result.setMsg(ResultCode.SUCCESS.getMsg());
         return result;
     }
+
+    @lombok.Data
+    public static class Data<T> {
+
+        private List<T> list;
+
+        private long total;
+
+    }
+
 }
