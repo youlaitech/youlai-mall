14e8724b7c3189dc2358879986561a8e4f554a4a
郝先瑞 <1490493387@qq.com>
2022-04-07 12:39:08 +0800

    docs(DataPermission.java): 数据权限补充参考链接

--- youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/annotation/DataPermission.java
+++ youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/annotation/DataPermission.java
@@ -4,6 +4,7 @@ import java.lang.annotation.*;
 
 /**
  * 数据权限注解
+ * 参考链接: https://gitee.com/baomidou/mybatis-plus/issues/I37I90
  *
  * @author <a href="mailto:2256222053@qq.com">zc</a>
  * @Date 2021-12-10 15:48
