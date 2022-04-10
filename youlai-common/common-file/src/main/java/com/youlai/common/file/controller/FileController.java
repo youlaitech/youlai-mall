477c3c87908bad463cb9a5a41e51a7eb61157468
郝先瑞 <1490493387@qq.com>
2022-04-10 11:24:29 +0800

    feat(minio): MinIO升级8.x版本，提取至公共API
    Closes #I51UMQ

---
+++ youlai-common/common-file/src/main/java/com/youlai/common/file/controller/FileController.java
@@ -0,0 +1,44 @@
+package com.youlai.common.file.controller;
+
+import com.youlai.common.file.service.MinioService;
+import com.youlai.common.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Api(tags = "文件接口")
+@RestController
+@RequestMapping("/api/v1/files")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final MinioService minioService;
+
+    @PostMapping
+    @ApiOperation(value = "文件上传")
+    @SneakyThrows
+    public Result<String> uploadFile(
+            @ApiParam("文件") @RequestParam(value = "file") MultipartFile file,
+            @ApiParam("存储桶名称(非必须，微服务有单独默认存储桶)") @RequestParam(value = "file", required = false) String bucketName
+    ) {
+        String path = minioService.putObject(file, bucketName);
+        return Result.success(path);
+    }
+
+    @DeleteMapping
+    @ApiOperation(value = "文件删除")
+    @SneakyThrows
+    public Result deleteFile(
+            @ApiParam("文件路径") @RequestParam String path) {
+        int lastIndex = path.lastIndexOf("/");
+        String bucket = path.substring(path.lastIndexOf("/", lastIndex - 1) + 1, lastIndex);
+        String fileName = path.substring(lastIndex + 1);
+        minioService.removeObject(bucket, fileName);
+        return Result.success();
+    }
+
+}
