9cb21396c7b89a040172834b1a8b5a8afc48d6f9
郝先瑞 <1490493387@qq.com>
2022-04-16 11:04:20 +0800

    refactor(PayTypeEnum.java): 支付类型枚举优化继承IBaseEnum接口

--- mall-oms/oms-api/src/main/java/com/youlai/mall/oms/enums/PayTypeEnum.java
+++ mall-oms/oms-api/src/main/java/com/youlai/mall/oms/enums/PayTypeEnum.java
@@ -1,41 +1,30 @@
 package com.youlai.mall.oms.enums;
 
 
+import com.youlai.common.base.IBaseEnum;
 import lombok.Getter;
 
 /**
  * @author huawei
- * @desc 订单来源类型枚举
+ * @desc 订单支付类型枚举
  * @email huawei_code@163.com
  * @date 2021/1/16
  */
-public enum PayTypeEnum {
+public enum PayTypeEnum implements IBaseEnum<Integer> {
 
-    WEIXIN_JSAPI(1,"微信JSAPI支付"),
-    ALIPAY(2,"支付宝支付"),
-    BALANCE(3,"会员余额支付"),
-    WEIXIN_APP(4,"微信APP支付");
+    WX_JSAPI(1, "微信JSAPI支付"),
+    ALIPAY(2, "支付宝支付"),
+    BALANCE(3, "会员余额支付"),
+    WX_APP(4, "微信APP支付");
 
-    PayTypeEnum(int code, String text) {
-        this.code = code;
-        this.text = text;
+    PayTypeEnum(int value, String label) {
+        this.value = value;
+        this.label = label;
     }
 
     @Getter
-    private Integer code;
+    private Integer value;
 
     @Getter
-    private String text;
-
-    public static PayTypeEnum getByCode(Integer code) {
-        PayTypeEnum payTypeEnum = null;
-
-        for (PayTypeEnum value : values()) {
-            if (value.getCode().equals(code)) {
-                payTypeEnum = value;
-            }
-        }
-        return payTypeEnum;
-    }
-
+    private String label;
 }
