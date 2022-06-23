c489364f0f213e6b72271ab78e847dde2ef03198
郝先瑞 <1490493387@qq.com>
2022-06-23 23:59:26 +0800

    docs(CouponForm.java): 优惠券表单对象注释完善

--- mall-sms/sms-boot/src/main/java/com/youlai/mall/sms/pojo/form/CouponForm.java
+++ mall-sms/sms-boot/src/main/java/com/youlai/mall/sms/pojo/form/CouponForm.java
@@ -1,98 +1,71 @@
 package com.youlai.mall.sms.pojo.form;
 
-import com.youlai.mall.sms.common.enums.CouponTypeEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.Date;
 
 /**
- * @author huawei
- * @desc 优惠券提交表单
- * @email huawei_code@163.com
- * @date 2021/3/14
+ * 优惠券表单对象
+ *
+ * @author haoxr
+ * @date 2022/6/23
  */
-@ApiModel("优惠券提交表单")
+@ApiModel("优惠券表单对象")
 @Data
 public class CouponForm {
 
-    /**
-     * ID
-     */
+    @ApiModelProperty("ID")
     private Long id;
 
-    /**
-     * 优惠券标题（有图片则显示图片）：无门槛50元优惠券 | 单品最高减2000元
-     */
-    @ApiModelProperty("优惠券标题")
-    @NotBlank(message = "请填写优惠券标题")
-    private String title;
+    @ApiModelProperty("优惠券名称")
+    private String name;
 
-    /**
-     * 图片
-     */
-    @ApiModelProperty("优惠券图片")
-    private String img;
+    @ApiModelProperty("优惠券类型(1:满减券;2:直减券;3:折扣券)")
+    private Integer type;
 
-    /**
-     * 1满减券 2叠加满减券 3无门槛券（需要限制大小）
-     */
-    @ApiModelProperty("优惠券类型")
-    private CouponTypeEnum type;
+    @ApiModelProperty("优惠券码")
+    private String code;
 
-    /**
-     * 满多少才可以使用（为0则不限制金额）
-     */
-    @ApiModelProperty("优惠券满减金额（为0则不限制金额）")
-    private Long conditionPrice;
+    @ApiModelProperty("优惠券状态(0:未发布;1:已发布;2:已结束;)")
+    private Integer status;
 
-    /**
-     * 抵扣价格
-     */
-    @ApiModelProperty("优惠券抵扣价格")
-    @NotNull(message = "请填写优惠券抵扣金额")
-    @Min(value = 1,message = "抵扣价格不能小于1")
-    private Long price;
+    @ApiModelProperty("使用平台(0:全部;1:移动端;2:PC;)")
+    private Integer platform;
 
-    /**
-     * 优惠券总量
-     */
-    @ApiModelProperty("优惠券总量")
-    @NotNull(message = "请填写优惠券总量")
-    @Min(value = 1,message = "优惠券总量不能小于1")
-    private Integer publishCount;
+    @ApiModelProperty("优惠券面值金额(单位:分)")
+    private Long faceValue;
 
-    /**
-     * 每张优惠券限领张数（默认为1，为0不限制）
-     */
-    @ApiModelProperty("优惠券限领张数")
-    @Min(value = 0,message = "优惠券限领张数不能为负数")
-    private Integer limitCount;
+    @ApiModelProperty("优惠券折扣")
+    private BigDecimal discount;
 
-    /**
-     * 发放开始时间
-     */
-    @ApiModelProperty("优惠券发放开始时间")
-    @NotNull(message = "请填写优惠券发放开始时间")
-    private Date startTime;
+    @ApiModelProperty("优惠券总数(0:无限制)")
+    private Integer totalCount;
 
-    /**
-     * 发放结束时间
-     */
-    @ApiModelProperty("优惠券发放结束时间")
-    @NotNull(message = "请填写优惠券发放结束时间")
-    private Date endTime;
+    @ApiModelProperty("使用门槛(0:无门槛)")
+    private Long minPoint;
 
-    /**
-     * 自领取之日起有效天数
-     */
+    @ApiModelProperty("每人限领张数(0:不限制)")
+    private Integer perLimit;
+
+    @ApiModelProperty("有效期类型(0:自领取之日起有效天数;1:有效起止时间)")
+    private Integer validType;
+
     @ApiModelProperty("自领取之日起有效天数")
-    @NotNull(message = "请填写正确的有效天数")
-    @Min(value = 1,message = "有效天数总量不能小于1")
     private Integer validDays;
+
+    @ApiModelProperty("有效期开始时间")
+    private Date validBeginTime;
+
+    @ApiModelProperty("有效期截止时间")
+    private Date validEndTime;
+
+    @ApiModelProperty("使用类型(0:全场通用;1:指定分类;2:指定商品)")
+    private Integer useType;
+
+    @ApiModelProperty("备注")
+    private String remark;
 
 }
