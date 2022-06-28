f9b5bbaa3ab96f7b50bbe899cbc2249ef9db362e
郝先瑞 <1490493387@qq.com>
2022-06-28 23:19:42 +0800

    style(SysRoleMenuServiceImpl): 删除无用引用

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/service/impl/SysRoleMenuServiceImpl.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/service/impl/SysRoleMenuServiceImpl.java
@@ -1,17 +1,12 @@
 package com.youlai.admin.service.impl;
 
-import cn.hutool.core.collection.CollectionUtil;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.youlai.admin.pojo.entity.SysRoleMenu;
 import com.youlai.admin.mapper.SysRoleMenuMapper;
+import com.youlai.admin.pojo.entity.SysRoleMenu;
 import com.youlai.admin.service.SysRoleMenuService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuMapper, SysRoleMenu> implements SysRoleMenuService {
