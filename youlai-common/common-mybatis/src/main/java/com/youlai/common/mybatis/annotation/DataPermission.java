53009a1422a4d73c4f9c02a5f13f621c62ac2409
郝先瑞 <1490493387@qq.com>
2022-07-31 15:49:22 +0800

    refactor(DataPermission.java): 完善注释

--- youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/annotation/DataPermission.java
+++ youlai-common/common-mybatis/src/main/java/com/youlai/common/mybatis/annotation/DataPermission.java
@@ -3,8 +3,9 @@ package com.youlai.common.mybatis.annotation;
 import java.lang.annotation.*;
 
 /**
- * 数据权限注解
- * 参考链接: https://gitee.com/baomidou/mybatis-plus/issues/I37I90
+ * MP数据权限注解
+ * <p>
+ * https://gitee.com/baomidou/mybatis-plus/issues/I37I90
  *
  * @author <a href="mailto:2256222053@qq.com">zc</a>
  * @Date 2021-12-10 15:48
