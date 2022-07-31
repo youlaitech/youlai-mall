99a743cdcc19822012f3bb0044973684f9690c20
郝先瑞 <1490493387@qq.com>
2022-07-31 15:47:28 +0800

    refactor: canal消息对象优化

--- youlai-common/common-web/src/main/java/com/youlai/common/web/domain/CanalMessage.java
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/domain/CanalMessage.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 /**
- * Canal 消息监听
+ * Canal 消息对象
  *
  * @author haoxr
  * @date 2021/11/4 23:59
@@ -15,26 +15,15 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class CanalMessage<T> {
-    @JsonProperty("type")
     private String type;
-    @JsonProperty("table")
     private String table;
-    @JsonProperty("data")
     private List<T> data;
-    @JsonProperty("database")
     private String database;
-    @JsonProperty("es")
     private Long es;
-    @JsonProperty("id")
     private Integer id;
-    @JsonProperty("isDdl")
     private Boolean isDdl;
-    @JsonProperty("old")
     private List<T> old;
-    @JsonProperty("pkNames")
     private List<String> pkNames;
-    @JsonProperty("sql")
     private String sql;
-    @JsonProperty("ts")
     private Long ts;
 }
