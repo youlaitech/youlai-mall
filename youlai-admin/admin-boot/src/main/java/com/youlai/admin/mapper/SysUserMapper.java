8f0475f304ddb97765f41d00e2de3111bdeb486d
郝先瑞 <1490493387@qq.com>
2022-08-19 23:52:46 +0800

    refactor: 移除数据权限

--- youlai-admin/admin-boot/src/main/java/com/youlai/admin/mapper/SysUserMapper.java
+++ youlai-admin/admin-boot/src/main/java/com/youlai/admin/mapper/SysUserMapper.java
@@ -29,7 +29,7 @@ public interface SysUserMapper extends BaseMapper<SysUser> {
      * @param queryParams 查询参数
      * @return
      */
-    @DataPermission(deptAlias = "d")
+    // @DataPermission(deptAlias = "d")
     Page<UserPO> listUserPages(Page<UserPO> page, UserPageQuery queryParams);
 
     /**
