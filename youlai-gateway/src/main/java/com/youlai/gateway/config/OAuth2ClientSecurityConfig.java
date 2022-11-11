f7f35b3af064f57eda0de65a64f281b0ce02cb6a
haoxr <1490493387@qq.com>
2022-11-12 00:09:26 +0800

    feat(OAuth2ClientSecurityConfig.java): Spring Cloud Gateway开启CORS资源共享允许跨域
    Closes #I60ESM

--- youlai-gateway/src/main/java/com/youlai/gateway/config/OAuth2ClientSecurityConfig.java
+++ youlai-gateway/src/main/java/com/youlai/gateway/config/OAuth2ClientSecurityConfig.java
@@ -1,16 +1,14 @@
 package com.youlai.gateway.config;
 
-import cn.hutool.core.convert.Convert;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 
 /**
@@ -23,19 +21,34 @@ import java.util.List;
 @Slf4j
 public class OAuth2ClientSecurityConfig {
 
-
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http
     ) {
         http.authorizeExchange()
-                .pathMatchers("/**").permitAll()
-                .anyExchange().authenticated()
+                .anyExchange().permitAll()
                 .and()
                 // 禁用csrf token安全校验
                 .csrf().disable();
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource (new PathPatternParser());
+        CorsConfiguration corsConfig = new CorsConfiguration();
 
+        // 允许所有请求方法
+        corsConfig.addAllowedMethod ("*");
+        // 允许所有域，当请求头
+        corsConfig.addAllowedOriginPattern ("*");
+        // 允许全部请求头
+        corsConfig.addAllowedHeader ("*");
+        // 允许携带 Authorization 头
+        corsConfig.setAllowCredentials (true);
+        // 允许全部请求路径
+        source.registerCorsConfiguration ("/**", corsConfig);
+
+        return source;
+    }
 
 }
