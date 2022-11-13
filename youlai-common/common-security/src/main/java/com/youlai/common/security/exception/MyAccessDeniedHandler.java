371a6570d6b0542187a5daa107592b54d3150755
haoxr <1490493387@qq.com>
2022-11-14 00:33:10 +0800

    feat: Spring Security添加自定义认证和访问异常

---
+++ youlai-common/common-security/src/main/java/com/youlai/common/security/exception/MyAccessDeniedHandler.java
@@ -0,0 +1,31 @@
+package com.youlai.common.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.youlai.common.result.Result;
+import com.youlai.common.result.ResultCode;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 自定义无权限异常
+ *
+ * @author haoxr
+ * @date 2022/11/13
+ */
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), Result.failed(ResultCode.ACCESS_UNAUTHORIZED));
+    }
+}
