4f843aec66880b2dea079cfa440987482fafd44c
haoxr <1490493387@qq.com>
2023-01-06 23:58:29 +0800

    docs: 代码注释优化

--- mall-oms/oms-api/src/main/java/com/youlai/mall/oms/api/OrderFeignClient.java
+++ mall-oms/oms-api/src/main/java/com/youlai/mall/oms/api/OrderFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * 订单 Feign Client
+ * 订单Feign Client
  *
  * @author haoxr
  * @date 2021/3/13
