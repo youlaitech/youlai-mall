318cd5d4ca40871eb2ddd2a3d3dd060fafecf049
郝先瑞 <1490493387@qq.com>
2023-04-13 21:27:15 +0800

    fix: `SpringCache`未关联`redisConnection`无效问题修复

--- youlai-common/common-redis/src/main/java/com/youlai/common/redis/RedisCacheConfig.java
+++ youlai-common/common-redis/src/main/java/com/youlai/common/redis/RedisCacheConfig.java
@@ -6,6 +6,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
@@ -18,6 +21,13 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @Configuration
 @EnableCaching
 public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory, CacheProperties cacheProperties){
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration(cacheProperties))
+                .build();
+    }
 
     @Bean
     RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
@@ -38,6 +48,7 @@ public class RedisCacheConfig {
         if (!redisProperties.isUseKeyPrefix()) {
             config = config.disableKeyPrefix();
         }
+        config = config.computePrefixWith(name -> name + ":");//覆盖默认key双冒号  CacheKeyPrefix#prefixed
         return config;
     }
 
