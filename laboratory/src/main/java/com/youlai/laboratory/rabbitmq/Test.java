d8f17e1a6980b1d04fb95f841c447f5e753d7c4b
郝先瑞 <1490493387@qq.com>
2023-04-22 23:26:24 +0800

    refactor: 删除无用代码

--- laboratory/src/main/java/com/youlai/laboratory/rabbitmq/Test.java
+++
@@ -1,8 +0,0 @@
-package com.youlai.laboratory.rabbitmq;
-
-/**
- * @author haoxr
- * @date 2021/11/28 10:06
- */
-public class Test {
-}
--- laboratory/src/main/java/com/youlai/laboratory/rocketmq/ext/TxRocketMQTemplate.java
+++
@@ -1,9 +0,0 @@
-package com.youlai.laboratory.rocketmq.ext;
-
-import org.apache.rocketmq.spring.annotation.ExtRocketMQTemplateConfiguration;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-
-@ExtRocketMQTemplateConfiguration
-public class TxRocketMQTemplate extends RocketMQTemplate {
-
-}
